22e811d6ef6bfd990ede21926926eafaa0751b04
xHyroM <generalkubo@gmail.com>
2023-05-07 11:04:49 +0200

    feat(diff): provide comment id

--- src/diff/webhooks.js
+++ src/diff/webhooks.js
@@ -1,11 +1,19 @@
 /**
  * @param {unknown} eventPayload
+ * @param {import("@octokit/action").RestEndpointMethodTypes["repos"]["createCommitComment"]["response"]["data"]} comment
  * @param {string} id
  * @param {string} token
  * @param {string} content
  * @param {string|null} threadId
  */
-export const send = async (eventPayload, id, token, content, threadId) => {
+export const send = async (
+  eventPayload,
+  comment,
+  id,
+  token,
+  content,
+  threadId
+) => {
   const url = `https://discord.com/api/webhooks/${id}/${token}${
     threadId ? `?thread_id=${threadId}` : ""
   }`;
@@ -13,10 +21,10 @@ export const send = async (eventPayload, id, token, content, threadId) => {
   if (content.length > 2000) {
     content = `${content.slice(
       0,
-      1860
+      1830
     )}...\n\`\`\`\n[You can see full comment here](https://github.com/xHyroM/discord-datamining/commit/${
       eventPayload.after
-    })`;
+    }#commitcomment-${comment.id})`;
   }
 
   const res = await fetch(url, {
