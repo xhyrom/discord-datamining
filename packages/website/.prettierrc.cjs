f1a92ca6299a421a9e4c6eea76d42fedcef1854f
xHyroM <generalkubo@gmail.com>
2023-05-28 22:53:19 +0200

    feat: init website

---
+++ packages/website/.prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    }
+  ],
+};
---
+++ packages/website/src/config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  name: "Discord Datamining",
+  description: "soon!",
+  themeColor: "#fbc11a",
+  origin: "https://discord-datamining.xhyrom.dev",
+};
