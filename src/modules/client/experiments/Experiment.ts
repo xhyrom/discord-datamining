7d7317a370c219881103e4b4eb8de47b4c46c118
xHyroM <generalkubo@gmail.com>
2023-06-11 12:08:14 +0200

    all

---
+++ src/modules/client/experiments/Experiment.ts
@@ -0,0 +1,139 @@
+import type {
+  Experiment as ExperimentType,
+  FeatureFilter,
+  Filter,
+  HubTypeFilter,
+  IDFilter,
+  IDRangeFilter,
+  MemberCountRangeFilter,
+  Population,
+  RangeByHashFilter,
+  VanityURLFilter,
+} from "./types";
+
+const andList = new Intl.ListFormat();
+const orList = new Intl.ListFormat("en", { type: "disjunction" });
+const hubTypes = ["Default", "High School", "College"];
+
+const nameFormat = (name: string) => {
+  return name === "none" ? "None" : `Treatment ${name}`;
+};
+
+export class Experiment {
+  public data: ExperimentType["data"];
+  public rollout: ExperimentType["rollout"];
+  #diff?: string | undefined;
+
+  constructor(experiment: ExperimentType) {
+    this.data = experiment.data;
+    this.rollout = experiment.rollout;
+  }
+
+  get hash() {
+    return this.data.hash;
+  }
+
+  get name() {
+    if (!this.data.label && !this.data.id) return `unknown ${this.data.hash}`;
+
+    return this.data.label
+      ? `${this.data.label} - ${this.data.id} (${this.data.hash})`
+      : `${this.data.id} (${this.data.hash})`;
+  }
+
+  get formattedPopulations() {
+    return this.formatPopulations(this.rollout?.populations ?? []);
+  }
+
+  get formattedOverrides() {
+    if (!this.rollout) return "";
+
+    return Object.entries(this.rollout.overrides).length > 0
+      ? Object.entries(this.rollout.overrides)
+          .map(
+            ([name, value]) => `**${nameFormat(name)}**: ${value.join(", ")}`
+          )
+          .join("\n\n")
+      : "Any overrides";
+  }
+
+  get formattedOverridesFormatted() {
+    return this.formatPopulations(this.rollout?.overrides_formatted ?? []);
+  }
+
+  get diff() {
+    return this.#diff;
+  }
+
+  set diff(diff: string | undefined) {
+    this.#diff = diff;
+  }
+
+  private formatPopulations(populations: Population[]) {
+    let format = "";
+
+    for (const population of populations) {
+      const filters =
+        population.filters.length !== 0
+          ? `**Filter**: ${andList.format(
+              population.filters.map((f) => this.parseFilter(f))
+            )}`
+          : "";
+
+      if (filters) format += `${filters}\n`;
+
+      for (const [bucketName, bucketValue] of Object.entries(
+        population.buckets
+      )) {
+        const percentage =
+          bucketValue.rollout.reduce(
+            (total, range) => total + range.end - range.start,
+            0
+          ) / 100;
+        format += `**${nameFormat(
+          bucketName
+        )}**: ${percentage}% (${bucketValue.rollout
+          .map((r) => `${r.start}-${r.end}`)
+          .join(", ")})\n`;
+      }
+
+      format += "\n";
+    }
+
+    return format.length > 1024 ? format.slice(0, 1021) + "..." : format;
+  }
+
+  private parseFilter(f: Filter) {
+    if (f.type === "guild_has_feature")
+      return `Server has feature ${orList.format(
+        (f as FeatureFilter).features
+      )}`;
+    if (f.type === "guild_id_range")
+      return `Server ID is in range ${(f as IDRangeFilter).min_id ?? 0} - ${
+        (f as IDRangeFilter).max_id ?? 0
+      }`;
+    if (f.type === "guild_member_count_range")
+      return `Server member count is ${
+        (f as MemberCountRangeFilter).max_count
+          ? `in range ${(f as MemberCountRangeFilter).min_count ?? 0} - ${
+              (f as MemberCountRangeFilter).max_count ?? 0
+            }`
+          : `${(f as MemberCountRangeFilter).min_count}+`
+      }`;
+    if (f.type === "guild_ids")
+      return `Server ID is ${orList.format((f as IDFilter).ids)}`;
+    if (f.type === "guild_hub_types")
+      return `Server hub type is ${orList.format(
+        (f as HubTypeFilter).hub_types.map((t) => hubTypes[t]!)
+      )}`;
+    if (f.type === "guild_has_vanity_url")
+      return `Server ${
+        (f as VanityURLFilter).has_vanity ? "has" : "does not have"
+      } a vanity URL`;
+    if (f.type === "guild_in_range_by_hash")
+      return `${(f as RangeByHashFilter).target / 100}% of servers (hash key ${
+        (f as RangeByHashFilter).hash_key
+      }, target ${(f as RangeByHashFilter).target})`;
+    return f.type;
+  }
+}
--- src/modules/client/experiments/index.ts
+++ src/modules/client/experiments/index.ts
@@ -1,7 +1,21 @@
 import { join } from "node:path";
 import type { Module } from "../../index.ts";
 import { Client } from "../index.ts";
-import { writeFile } from "../../../utils.ts";
+import {
+  chunk,
+  formatNumber,
+  getWebhookFromEnv,
+  maximumStringLen,
+  octokit,
+  postToDiscord,
+  pushToGit,
+  readFile,
+  writeFile,
+} from "../../../utils.ts";
+import { Experiment } from "./Experiment.ts";
+import deepEqual from "fast-deep-equal";
+import type { APIEmbed, APIEmbedField } from "discord-api-types/v10";
+import { EmbedBuilder } from "@discordjs/builders";
 
 export class Experiments implements Module {
   get baseDir() {
@@ -17,6 +31,10 @@ export class Experiments implements Module {
       return;
     }
 
+    const oldExperiments = JSON.parse(
+      (await readFile(join(this.baseDir, "experiments.json"))) ?? "[]"
+    ).map((e: any) => new Experiment(e));
+
     await writeFile(
       join(this.baseDir, "experiments.json"),
       JSON.stringify(experiments, null, 2)
@@ -28,15 +46,174 @@ export class Experiments implements Module {
         JSON.stringify(experiment, null, 2)
       );
     }
+
+    const result = await pushToGit(
+      `🧪 Experiments were updated`,
+      `Experiments (${formatNumber(experiments.length)}):\n${experiments
+        .map((experiment) => experiment.name)
+        .join("\n")}`
+    );
+
+    if (!result?.update?.hash) return;
+
+    const diff = await this.diff(
+      result.update.hash.from,
+      result.update.hash.to,
+      oldExperiments,
+      experiments
+    );
+
+    const embeds: APIEmbed[] = [];
+
+    for (const post of diff.addedExperiments) {
+      embeds.push(this.buildEmbed("New", post).setColor(0x2cde5c).toJSON());
+    }
+
+    for (const post of diff.removedExperiments) {
+      embeds.push(this.buildEmbed("Removed", post).setColor(0xde2c2c).toJSON());
+    }
+
+    for (const post of diff.updatedExperiments) {
+      if (!post.diff) continue;
+
+      embeds.push(
+        this.buildEmbed("Updated", post)
+          .setDescription(
+            maximumStringLen(`\`\`\`diff\n${post.diff}\n\`\`\``, 4096)
+          )
+          .setColor(0x2c5cde)
+          .toJSON()
+      );
+    }
+
+    const embedsPerTen = chunk(embeds, 10);
+
+    for (const embeds of embedsPerTen) {
+      await postToDiscord(
+        getWebhookFromEnv("DISCORD_WEBHOOK_EXPERIMENTS"),
+        result?.update?.hash.to,
+        {
+          content: "<@&1105589221185568851>",
+          embeds,
+        }
+      );
+    }
   }
 
-  async experiments() {
+  async experiments(): Promise<Experiment[] | null> {
     const experiments = await fetch(
       "https://api.distools.xhyrom.dev/v2/experiments?also_with_unknown_ids=true"
     );
     if (!experiments.ok) return null;
 
     const json = await experiments.json();
-    return json;
+    return json.map((e: any) => new Experiment(e));
+  }
+
+  private buildEmbed(action: string, experiment: Experiment) {
+    const fields = [
+      {
+        name: "Label",
+        value: experiment.data.label ?? "unknown",
+        inline: true,
+      },
+      {
+        name: "Id",
+        value: experiment.data.id ?? "unknown",
+        inline: true,
+      },
+      {
+        name: "Hash",
+        value: experiment.data.hash.toString(),
+        inline: true,
+      },
+      {
+        name: "Kind",
+        value: experiment.data.kind,
+        inline: true,
+      },
+      {
+        name: "Treatments",
+        value: experiment.data.description?.join?.("\n"),
+      },
+      {
+        name: "Populations",
+        value: experiment.formattedPopulations,
+      },
+      {
+        name: "Overrides",
+        value: experiment.formattedOverrides,
+      },
+      {
+        name: "Overrides Formatted",
+        value: experiment.formattedOverridesFormatted,
+      },
+    ];
+
+    // Keep only fields with a value
+    const possibleFields = fields.filter((f) => f.value) as APIEmbedField[];
+
+    return new EmbedBuilder()
+      .setTitle(`${action} Experiment`)
+      .addFields(possibleFields);
+  }
+
+  private async diff(
+    before: string,
+    after: string,
+    oldExperiments: Experiment[],
+    newExperiments: Experiment[]
+  ): Promise<{
+    removedExperiments: Experiment[];
+    updatedExperiments: Experiment[];
+    addedExperiments: Experiment[];
+  }> {
+    const diff = await octokit.repos.compareCommits({
+      owner: "xHyroM",
+      repo: "discord-datamining",
+      base: before,
+      head: after,
+    });
+
+    const removedExperiments = [];
+    const updatedExperiments = [];
+    const addedExperiments = [];
+
+    for (const oldExperiment of oldExperiments) {
+      const newExperiment = newExperiments.find(
+        (a) => a.hash === oldExperiment.hash
+      );
+
+      if (!newExperiment) {
+        removedExperiments.push(oldExperiment);
+        continue;
+      }
+
+      if (!deepEqual(oldExperiment, newExperiment)) {
+        newExperiment.diff = diff.data.files?.find(
+          (f) =>
+            f.filename ===
+            `data/client/experiments/${newExperiment.hash}/data.json`
+        )?.patch;
+
+        updatedExperiments.push(newExperiment);
+      }
+    }
+
+    for (const newExperiment of newExperiments) {
+      const oldExperiment = oldExperiments.find(
+        (a) => a.hash === newExperiment.hash
+      );
+
+      if (!oldExperiment) {
+        addedExperiments.push(newExperiment);
+      }
+    }
+
+    return {
+      removedExperiments,
+      updatedExperiments,
+      addedExperiments,
+    };
   }
 }
