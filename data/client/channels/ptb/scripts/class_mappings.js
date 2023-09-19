f929b3644891d31aff4ae82ca43479487823a1c7
xHyroM <github@xhyrom.dev>
2023-09-19 20:56:04 +0000

    📥 Public Testing Beta Build 229406 (0667e8a583e63d95b140e4c7486d2d6456ef0b5f)
    Build Number: 229406
    Version Hash: 0667e8a583e63d95b140e4c7486d2d6456ef0b5f
    Build At: 19/8/2023 20:41:1 (1695156061561)
    
    Scripts (4):
      53e6831c93774728815e.js
      6df35aaf9c9a1fae49bc.js
      3f1d253fa8d4496bc05e.js
    * c6bbcd611a0fa0e89598.js
    
    Stylesheets (1):
    * 40532.14d7a72e7a7d11e4a6c4.css

--- data/client/channels/ptb/scripts/class_mappings.js
+++ data/client/channels/ptb/scripts/class_mappings.js
@@ -10498,6 +10498,7 @@
                 container: "container-2qjEOK",
                 header: "header-elFGge",
                 sidebar: "sidebar-2rB69-",
+                scroller: "scroller-qwiOD6",
                 page: "page-2dwf_i",
                 innerHeader: "innerHeader-216XtR"
             }
@@ -26735,4 +26736,4 @@
         }
     }
 ]);
-//# sourceMappingURL=a796cfc46519c6f92598.js.map
+//# sourceMappingURL=6df35aaf9c9a1fae49bc.js.map
