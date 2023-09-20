6f3d8d295d5122e892210da6e9904c5f257a5899
xHyroM <github@xhyrom.dev>
2023-09-20 20:48:48 +0000

    📥 Public Testing Beta Build 229775 (3727566e30c883ece0d78667d492c0916d23582f)
    Build Number: 229775
    Version Hash: 3727566e30c883ece0d78667d492c0916d23582f
    Build At: 20/8/2023 20:35:6 (1695242106812)
    
    Scripts (4):
      8a0157c389108d7b0a39.js
      0be372ae62c9a9272b29.js
      69ce3ce09ac5d12d457f.js
    * 7cc6dc9db1c7aea34fb8.js
    
    Stylesheets (1):
    * 40532.6a770c1a352d374523c5.css

--- data/client/channels/ptb/scripts/class_mappings.js
+++ data/client/channels/ptb/scripts/class_mappings.js
@@ -18898,6 +18898,17 @@
                 secondaryButton: "secondaryButton-b2iEgd"
             }
         },
+        816529: e => {
+            e.exports = {
+                premiumTrialBadge: "premiumTrialBadge-WjIyAB",
+                premiumTrialBadgeIcon: "premiumTrialBadgeIcon-1Y_EPl",
+                premiumTrialTier0UnacknowledgedBadge: "premiumTrialTier0UnacknowledgedBadge-1uVRwo",
+                premiumDiscountUnacknowledgedBadge: "premiumDiscountUnacknowledgedBadge-11ov_R",
+                premiumTrialTier2UnacknowledgedBadge: "premiumTrialTier2UnacknowledgedBadge-1iHCWR",
+                premiumTrialAcknowledgedBadge: "premiumTrialAcknowledgedBadge-2fhKRM",
+                premiumTrialBadgeSelected: "premiumTrialBadgeSelected-3Qgd2d"
+            }
+        },
         189532: e => {
             e.exports = {
                 headerBackground: "headerBackground-131uxy",
@@ -19378,16 +19389,6 @@
                 giftCopy: "giftCopy-1CPzXM",
                 giftCopyHeading: "giftCopyHeading-BAlAZB",
                 giftButtonText: "giftButtonText-3_Cv78"
-            }
-        },
-        771638: e => {
-            e.exports = {
-                premiumTrialBadge: "premiumTrialBadge-pEqntF",
-                premiumTrialBadgeIcon: "premiumTrialBadgeIcon-24nUOG",
-                premiumTrialTier0UnacknowledgedBadge: "premiumTrialTier0UnacknowledgedBadge-Fhgvyi",
-                premiumTrialTier2UnacknowledgedBadge: "premiumTrialTier2UnacknowledgedBadge-1XA1ny",
-                premiumTrialAcknowledgedBadge: "premiumTrialAcknowledgedBadge-2QxbU_",
-                premiumTrialBadgeSelected: "premiumTrialBadgeSelected-3NVwA4"
             }
         },
         42707: e => {
@@ -26807,4 +26808,4 @@
         }
     }
 ]);
-//# sourceMappingURL=35a9cc21eea0e2e661c9.js.map
+//# sourceMappingURL=0be372ae62c9a9272b29.js.map
