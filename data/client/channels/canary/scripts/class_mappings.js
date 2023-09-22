ea12f854c017da6a71d3aac4e6dc33136d87dcd4
xHyroM <github@xhyrom.dev>
2023-09-22 22:34:39 +0000

    📥 Canary Build 230712 (abc49d72259f1f7862b47849fbc8d34d4889a920)
    Build Number: 230712
    Version Hash: abc49d72259f1f7862b47849fbc8d34d4889a920
    Build At: 22/8/2023 22:21:49 (1695421309315)
    
    Scripts (4):
      7a0f9bb6477879fcb20b.js
      76e8d5661bedc07e7520.js
      f44b7278715fe39fd0fe.js
    * 8279fecbc3bd51e49b6a.js
    
    Stylesheets (1):
    * 40532.87a570163691b334d5ec.css

--- data/client/channels/canary/scripts/class_mappings.js
+++ data/client/channels/canary/scripts/class_mappings.js
@@ -7736,6 +7736,18 @@
                 arrow: "arrow-3MeJHj"
             }
         },
+        516499: e => {
+            e.exports = {
+                header: "header-2Yd2Hh",
+                subheader: "subheader-18b4EV",
+                closeButton: "closeButton-3evMqZ",
+                button: "button-2w-KF8",
+                image: "image-3ZkQfv",
+                video: "video-3ULxIr",
+                media: "media-1EIjIn",
+                mediaContainer: "mediaContainer-1GNZ1u"
+            }
+        },
         421657: e => {
             e.exports = {
                 accessory: "accessory-A8wSuS",
@@ -26855,4 +26867,4 @@
         }
     }
 ]);
-//# sourceMappingURL=e587c0dcff5d358faead.js.map
+//# sourceMappingURL=76e8d5661bedc07e7520.js.map
