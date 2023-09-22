e232148658a0654d45a09b5f0cb4441ebd54f92d
xHyroM <github@xhyrom.dev>
2023-09-22 20:23:03 +0000

    📥 Public Testing Beta Build 230601 (b4a58f68a0b9dcf75673936e1ac605bfc9689de2)
    Build Number: 230601
    Version Hash: b4a58f68a0b9dcf75673936e1ac605bfc9689de2
    Build At: 22/8/2023 20:8:22 (1695413302577)
    
    Scripts (4):
      6b6b265a5cf2a5b0b36e.js
      7919ff6977954c5e319b.js
      f026d60904c94c5806a5.js
    * 5e585fa801b853fcdc1c.js
    
    Stylesheets (1):
    * 40532.ecd27290063354e23682.css

--- data/client/channels/ptb/scripts/vendor.js
+++ data/client/channels/ptb/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 5c48bbd65b559ebb2094.js.LICENSE.txt */
+/*! For license information please see f026d60904c94c5806a5.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -37035,7 +37035,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-6e36d0893a1f9e25efa658b15a2ec126bea24e9e",
+                    release: "discord_web-b4a58f68a0b9dcf75673936e1ac605bfc9689de2",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37062,8 +37062,8 @@
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "230590") ? t : "");
-                X.YA("builtAt", String("1695412297444"));
+                X.YA("buildNumber", null !== (t = "230601") ? t : "");
+                X.YA("builtAt", String("1695413302577"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
@@ -48442,4 +48442,4 @@
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=5c48bbd65b559ebb2094.js.map
+//# sourceMappingURL=f026d60904c94c5806a5.js.map
