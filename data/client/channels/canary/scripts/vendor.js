7846f912c32bf259a88863af0e12e9690f612fd8
xHyroM <github@xhyrom.dev>
2023-10-06 16:50:06 +0000

    📥 Canary Build 235231 (0fa444b74de0525d304047a2508fae69c5b0dfac)
    Build Number: 235231
    Version Hash: 0fa444b74de0525d304047a2508fae69c5b0dfac
    Build At: 6/9/2023 16:33:50 (1696610030578)
    
    Scripts (4):
      d7ff4995189965997e52.js
      ea6cbebf908ffc84fd6f.js
      7e78b6c2cde97f37cbd3.js
    * 6054904632182f327529.js
    
    Stylesheets (1):
    * 40532.5ca63096d44d8077c153.css

--- data/client/channels/canary/scripts/vendor.js
+++ data/client/channels/canary/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 1197762210749486be26.js.LICENSE.txt */
+/*! For license information please see 7e78b6c2cde97f37cbd3.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -37093,7 +37093,7 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-a7f264bb1df285bcfd7083a42ee60a2dae6c3442",
+                    release: "discord_web-0fa444b74de0525d304047a2508fae69c5b0dfac",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37120,8 +37120,8 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "235230") ? t : "");
-                X.YA("builtAt", String("1696609788260"));
+                X.YA("buildNumber", null !== (t = "235231") ? t : "");
+                X.YA("builtAt", String("1696610030578"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
@@ -48501,4 +48501,4 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=1197762210749486be26.js.map
+//# sourceMappingURL=7e78b6c2cde97f37cbd3.js.map
