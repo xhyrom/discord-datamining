4c9a9d3f5b4ba2fedf467963387d2d42ce88b926
xHyroM <github@xhyrom.dev>
2023-10-06 17:25:24 +0000

    📥 Canary Build 235248 (280c9e32c578fdd1eda7f1417915226ea27c0a60)
    Build Number: 235248
    Version Hash: 280c9e32c578fdd1eda7f1417915226ea27c0a60
    Build At: 6/9/2023 17:2:44 (1696611764709)
    
    Scripts (4):
      52db3612c87f23f4c91b.js
      ea6cbebf908ffc84fd6f.js
      886493dc05b08c9c3db3.js
    * 5fcfc52cc679c972ea86.js
    
    Stylesheets (1):
    * 40532.5ca63096d44d8077c153.css

--- data/client/channels/canary/scripts/vendor.js
+++ data/client/channels/canary/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 7e78b6c2cde97f37cbd3.js.LICENSE.txt */
+/*! For license information please see 886493dc05b08c9c3db3.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -37093,7 +37093,7 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-0fa444b74de0525d304047a2508fae69c5b0dfac",
+                    release: "discord_web-280c9e32c578fdd1eda7f1417915226ea27c0a60",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37120,8 +37120,8 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "235231") ? t : "");
-                X.YA("builtAt", String("1696610030578"));
+                X.YA("buildNumber", null !== (t = "235248") ? t : "");
+                X.YA("builtAt", String("1696611764709"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
@@ -37782,7 +37782,7 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
                     [P.MOMO_WALLET, "momo_wallet"],
                     [P.KAKAOPAY, "kakaopay"],
                     [P.GOPAY_WALLET, "gopay_wallet"],
-                    [P.CASH_APP, "cash_app"]
+                    [P.CASH_APP, "cashapp"]
                 ]),
                 w = new Map([
                     [P.BANCONTACT, "bancontact"],
@@ -48501,4 +48501,4 @@ sentry_client:`${e.name}/${e.version}`}})}(t,n)}`
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=7e78b6c2cde97f37cbd3.js.map
+//# sourceMappingURL=886493dc05b08c9c3db3.js.map
