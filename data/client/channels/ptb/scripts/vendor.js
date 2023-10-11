86d940d17e1d4258ce218cbfbaa2b4b5112151c2
xHyroM <github@xhyrom.dev>
2023-10-11 15:07:53 +0000

    📥 Public Testing Beta Build 236211 (5aa67d71c7f92af3debd0b6a14b7b2e84a111d1a)
    Build Number: 236211
    Version Hash: 5aa67d71c7f92af3debd0b6a14b7b2e84a111d1a
    Build At: 11/9/2023 14:52:18 (1697035938183)
    
    Scripts (4):
      25286818aa2274935816.js
      d193dc94b3233869b6d8.js
      5758f8ead9907fb7c6ca.js
    * 51b6c0d53d8de6389b20.js
    
    Stylesheets (1):
    * 40532.27ca15dbe0a05a4b5019.css

--- data/client/channels/ptb/scripts/vendor.js
+++ data/client/channels/ptb/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see abde160eeda4705aedb8.js.LICENSE.txt */
+/*! For license information please see 5758f8ead9907fb7c6ca.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -24343,6 +24343,7 @@
                 t.SPAM_MESSAGE_REQUESTS_VIEWED = "spam_message_requests_viewed";
                 t.MESSAGE_REQUESTS_VIEWED = "message_requests_viewed";
                 t.MESSAGE_REQUEST_PREVIEW_VIEWED = "message_request_preview_viewed";
+                t.EXPLICIT_MEDIA_OBSCURITY_TOGGLE = "explicit_media_obscurity_toggle";
                 t.DOWNLOAD_APP = "download_app";
                 t.PAYMENT_FLOW_STARTED = "payment_flow_started";
                 t.PAYMENT_FLOW_COMPLETED = "payment_flow_completed";
@@ -37112,7 +37113,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-6cb8f8eb3dc3e17e16ddba99187de8e026f5c5d5",
+                    release: "discord_web-5aa67d71c7f92af3debd0b6a14b7b2e84a111d1a",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37139,8 +37140,8 @@
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                X.YA("buildNumber", null !== (t = "236208") ? t : "");
-                X.YA("builtAt", String("1697034577693"));
+                X.YA("buildNumber", null !== (t = "236211") ? t : "");
+                X.YA("builtAt", String("1697035938183"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) X.YA(r, e[r]);
@@ -48537,4 +48538,4 @@
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=abde160eeda4705aedb8.js.map
+//# sourceMappingURL=5758f8ead9907fb7c6ca.js.map
