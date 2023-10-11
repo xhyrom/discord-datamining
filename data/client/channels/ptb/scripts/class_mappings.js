2f03627bcf6f31f5afa4748399ad83f5807d04d4
xHyroM <github@xhyrom.dev>
2023-10-11 17:38:03 +0000

    📥 Public Testing Beta Build 236267 (8d76bf4425f38b99cb81ae0f51cc970b6296f42a)
    Build Number: 236267
    Version Hash: 8d76bf4425f38b99cb81ae0f51cc970b6296f42a
    Build At: 11/9/2023 17:23:8 (1697044988384)
    
    Scripts (4):
      cf13cdc99129da846d36.js
      959c9c559ea12132e4b1.js
      f9c98b128fc100ddc4a3.js
    * 0087d0e305a19f015759.js
    
    Stylesheets (1):
    * 40532.f5d855a628112e5b9f9c.css

--- data/client/channels/ptb/scripts/class_mappings.js
+++ data/client/channels/ptb/scripts/class_mappings.js
@@ -6342,6 +6342,7 @@
                 serverSelectTitle: "serverSelectTitle-AQrb3T",
                 serverSelectDescription: "serverSelectDescription-Ij3a7x",
                 serverSelectField: "serverSelectField-1A-Gi1",
+                selectGuildErrorLabel: "selectGuildErrorLabel-1eQ3Hq",
                 confirmationSlide: "confirmationSlide-1q7h9r",
                 appliedToLabel: "appliedToLabel-2Am_xm",
                 guildButton: "guildButton-2lrOPG",
@@ -27108,4 +27109,4 @@
         }
     }
 ]);
-//# sourceMappingURL=fdcc8546daee9852a3bf.js.map
+//# sourceMappingURL=959c9c559ea12132e4b1.js.map
