564631891a9bee322d8fb2fac5ce73faccc066e1
xHyroM <github@xhyrom.dev>
2023-10-11 22:12:12 +0000

    📥 Stable Build 236418 (eeee57510e65e75426e6f6a5a1b7ae144d605bbf)
    Build Number: 236418
    Version Hash: eeee57510e65e75426e6f6a5a1b7ae144d605bbf
    Build At: 11/9/2023 21:30:46 (1697059846950)
    
    Scripts (4):
      4c6bedfc34e76460ade8.js
      4efa8761ce370c563248.js
      8a496270ccd680283176.js
    * 675ae39865fd3f811445.js
    
    Stylesheets (1):
    * 40532.bc56cc398b6bb0587e43.css

--- data/client/channels/stable/scripts/class_mappings.js
+++ data/client/channels/stable/scripts/class_mappings.js
@@ -6042,27 +6042,6 @@
                 fadeOut: "fadeOut-3CmRbO"
             }
         },
-        895107: e => {
-            e.exports = {
-                clipItem: "clipItem-3fKTW2",
-                buttonContainer: "buttonContainer-2DnFpI",
-                newIcon: "newIcon-3qyXQQ",
-                clipBadges: "clipBadges-3SqEsf",
-                clipBadge: "clipBadge-eRKwdM",
-                clipNewBadge: "clipNewBadge-c99euT clipBadge-eRKwdM",
-                clipDurationBadge: "clipDurationBadge-2GUmDx clipBadge-eRKwdM",
-                clipDurationEditIcon: "clipDurationEditIcon-3SqknF",
-                clipThumb: "clipThumb-1PCUzz",
-                clipThumbContainer: "clipThumbContainer-3aFZ2y",
-                clipFooter: "clipFooter-34yjBL",
-                clipMetadata: "clipMetadata-3jhdkN",
-                clipName: "clipName-Q83UIm",
-                clipTitle: "clipTitle-zkDBCw",
-                usersAndDelete: "usersAndDelete-2nIrRW",
-                button: "button-1-uzof",
-                miniIcon: "miniIcon-2oK0u_"
-            }
-        },
         933568: e => {
             e.exports = {
                 mediumBreakPoint: "920px",
@@ -6221,6 +6200,27 @@
                 title: "title-_uSBQv",
                 userCountPill: "userCountPill-2DVO59",
                 userCountIcon: "userCountIcon-tRq2vz"
+            }
+        },
+        359064: e => {
+            e.exports = {
+                clipItem: "clipItem-3C35Ec",
+                buttonContainer: "buttonContainer-xgRHlG",
+                newIcon: "newIcon-RHqgRF",
+                clipBadges: "clipBadges-pgPUWI",
+                clipBadge: "clipBadge-3UcfAK",
+                clipNewBadge: "clipNewBadge-2wtTbP clipBadge-3UcfAK",
+                clipDurationBadge: "clipDurationBadge-WP8jOr clipBadge-3UcfAK",
+                clipDurationEditIcon: "clipDurationEditIcon-3KKvNy",
+                clipThumb: "clipThumb-2dUNyk",
+                clipThumbContainer: "clipThumbContainer-orlvSg",
+                clipFooter: "clipFooter-2cmlwX",
+                clipMetadata: "clipMetadata-3VIMh-",
+                clipName: "clipName-19oBTM",
+                clipTitle: "clipTitle-3T5TWz",
+                usersAndDelete: "usersAndDelete-vhdJvn",
+                button: "button-2yQUTy",
+                miniIcon: "miniIcon-O9W7vb"
             }
         },
         241657: e => {
@@ -20933,6 +20933,15 @@
                 alt: "alt-3_Cl_W"
             }
         },
+        162630: e => {
+            e.exports = {
+                strangerDangerBanner: "strangerDangerBanner-2wxVfR",
+                safetyShieldIcon: "safetyShieldIcon-1zLay_",
+                buttons: "buttons-LfwThR",
+                shieldAndHeading: "shieldAndHeading-21F2Rf",
+                closeButton: "closeButton-1VfP30"
+            }
+        },
         103893: e => {
             e.exports = {
                 sharedCanvas: "sharedCanvas-1l4N32"
@@ -27115,4 +27124,4 @@
         }
     }
 ]);
-//# sourceMappingURL=5e91559cb980677f640c.js.map
+//# sourceMappingURL=4efa8761ce370c563248.js.map
