885859e4c48971384bec459b5c4c6191395525d2
xHyroM <github@xhyrom.dev>
2023-10-19 19:22:57 +0000

    📥 Canary Build 238710 (6048ccbe66113fb54e526ff488fea3183847c522)
    Build Number: 238710
    Version Hash: 6048ccbe66113fb54e526ff488fea3183847c522
    Build At: 19/9/2023 19:10:9 (1697742609424)
    
    Scripts (4):
      c83efb99dcbc13911df9.js
      ca3b3b4b9c51e470aa53.js
      12a7fc7a6a2698a93136.js
    * 3d29d25ad748c827aec7.js
    
    Stylesheets (1):
    * 40532.90f92d84362dcd324342.css

--- data/client/channels/canary/scripts/vendor.js
+++ data/client/channels/canary/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 025e27bc5711e9508c3a.js.LICENSE.txt */
+/*! For license information please see 12a7fc7a6a2698a93136.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -24715,6 +24715,7 @@
                 t.CLIP_SHARED = "clip_shared";
                 t.CLIPS_HARDWARE_CLASSIFICATION = "clips_hardware_classification";
                 t.CLIPS_ENABLED_TOGGLED = "clips_enabled_toggled";
+                t.CLIPS_SETTINGS_UPDATED = "clips_settings_updated";
                 t.STANDALONE_BILLING_FLOW_STARTED = "standalone_billing_flow_started";
                 t.STANDALONE_MANAGE_SUBSCRIPTION_PAGE_VIEWED = "standalone_manage_subscription_page_viewed";
                 t.INBOUND_PROMOTION_VIEWED = "inbound_promotion_viewed";
@@ -37151,7 +37152,7 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-a16e9a6feec94f7afc0786d2113e98762904c62d",
+                    release: "discord_web-6048ccbe66113fb54e526ff488fea3183847c522",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37178,8 +37179,8 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                Bt.YA("buildNumber", null !== (t = "238705") ? t : "");
-                Bt.YA("builtAt", String("1697742089326"));
+                Bt.YA("buildNumber", null !== (t = "238710") ? t : "");
+                Bt.YA("builtAt", String("1697742609424"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) Bt.YA(r, e[r]);
@@ -48579,4 +48580,4 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=025e27bc5711e9508c3a.js.map
+//# sourceMappingURL=12a7fc7a6a2698a93136.js.map
