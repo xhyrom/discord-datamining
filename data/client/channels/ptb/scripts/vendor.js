2f25737297774779e7fa26fb5d2121195fd62b98
xHyroM <github@xhyrom.dev>
2023-10-20 14:24:18 +0000

    📥 Public Testing Beta Build 239011 (cf70295cbccfc9a093d09c4a07dc99fc12c24dea)
    Build Number: 239011
    Version Hash: cf70295cbccfc9a093d09c4a07dc99fc12c24dea
    Build At: 20/9/2023 14:8:51 (1697810931322)
    
    Scripts (4):
      59a902cdf9e71daa6680.js
      c314e4c421cad9607b51.js
      523422406d412e733625.js
    * e3314ee25166843f5d2b.js
    
    Stylesheets (1):
    * 40532.d053ca8cca61d3caca6f.css

--- data/client/channels/ptb/scripts/vendor.js
+++ data/client/channels/ptb/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 4a28ae3ce9db7d7a9520.js.LICENSE.txt */
+/*! For license information please see 523422406d412e733625.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -37152,7 +37152,7 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-405d7a8520b674818d483254360d565aa408fec4",
+                    release: "discord_web-cf70295cbccfc9a093d09c4a07dc99fc12c24dea",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37179,8 +37179,8 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                Bt.YA("buildNumber", null !== (t = "239004") ? t : "");
-                Bt.YA("builtAt", String("1697783178069"));
+                Bt.YA("buildNumber", null !== (t = "239011") ? t : "");
+                Bt.YA("builtAt", String("1697810931322"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) Bt.YA(r, e[r]);
@@ -48580,4 +48580,4 @@ _.jH)(t)}.\nUrl: ${I(t)}`);
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=4a28ae3ce9db7d7a9520.js.map
+//# sourceMappingURL=523422406d412e733625.js.map
