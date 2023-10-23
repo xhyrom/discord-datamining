fd6061f649b2aca4b79b19e2d1ce290e82d87fee
xHyroM <github@xhyrom.dev>
2023-10-23 20:45:01 +0000

    📥 Canary Build 239591 (d85b92286721ff1dff3934cb0c60102bf2219493)
    Build Number: 239591
    Version Hash: d85b92286721ff1dff3934cb0c60102bf2219493
    Host Version: 1.0.82
    Build At: 23/9/2023 20:30:54 (1698093054416)
    
    Scripts (4):
      992a133350e9a94c1d97.js
      af178d456ad69fb97460.js
      00e12f537c579a20f110.js
    * b578f8e2758651e543d9.js
    
    Stylesheets (1):
    * 40532.63cfc1e688e86aa6bcca.css

--- data/client/channels/canary/scripts/vendor.js
+++ data/client/channels/canary/scripts/vendor.js
@@ -1,4 +1,4 @@
-/*! For license information please see 0cdab9220f1ed3874518.js.LICENSE.txt */
+/*! For license information please see 00e12f537c579a20f110.js.LICENSE.txt */
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
     [22897], {
         151131: (t, e, r) => {
@@ -37153,7 +37153,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-9b38fd8d8453659ffba0359d04bbefa2111a9572",
+                    release: "discord_web-d85b92286721ff1dff3934cb0c60102bf2219493",
                     beforeSend: function(t) {
                         return function(t) {
                             return null != t.exception && null != t.exception.values && t.exception.values.every((function(t) {
@@ -37180,8 +37180,8 @@
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
                 });
                 var t;
-                Bt.YA("buildNumber", null !== (t = "239588") ? t : "");
-                Bt.YA("builtAt", String("1698092643278"));
+                Bt.YA("buildNumber", null !== (t = "239591") ? t : "");
+                Bt.YA("builtAt", String("1698093054416"));
                 var e = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != e && "object" == typeof e)
                     for (var r in e) Bt.YA(r, e[r]);
@@ -48686,4 +48686,4 @@
         e(128594), e(535666), e(784633), e(678991)
     }
 ]);
-//# sourceMappingURL=0cdab9220f1ed3874518.js.map
+//# sourceMappingURL=00e12f537c579a20f110.js.map
