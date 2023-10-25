beb2c46e3b76690983275ad2c6515e7b35b71f45
xHyroM <github@xhyrom.dev>
2023-10-25 21:16:43 +0000

    📥 Public Testing Beta Build 240467 (f903260a28936cc595e92e685e333c2970d57d03)
    Build Number: 240467
    Version Hash: f903260a28936cc595e92e685e333c2970d57d03
    Host Version: 1.0.1038
    Build At: 25/9/2023 21:8:10 (1698268090847)
    
    Scripts (103):
      2c8b6326c4913ed66420.js
      68ac062a2160f436183f.js
      29bda2d7fef4a4f5b599.js
      2740ede9403c7526f84d.js
      79c892edd26d34740925.js
      0b2d3b11fe89441e9a68.js
      3d86c5d7d4a7d10dfd01.js
      94e9e8b3bbf30f475165.js
      4084926f980976ae3509.js
      acaf34c8ae2e6364014a.js
      6754459bfa4b06a0ce42.js
      2ad9b9a2a424b7fb294d.js
      80d55722485c0c45c3f6.js
      140339c75158017cbd89.js
      1bcd78fc1311157eceeb.js
      6f6aa3f097dd5dd2633e.js
      9a87c6d4fc22807915fc.js
      28cbbf53e79cf7167475.js
      f94ddfffe27102a38e07.js
      7f335bf966ae4beb1fdb.js
      3a68b3916ecf07a1a230.js
      7f19cfd25c2efa0c0e67.js
      9fa303621ddf0bf7272e.js
      fe753e272fca732721e3.js
      16a129af7446703c7922.js
      3bb34a166eaaa8d5569c.js
      0f3dd8bc85c3a66f1c7a.js
      9542ad1ed6d0bccb3cb8.js
      cc65787c5a49e10d1902.js
      33d6aeac832dce305350.js
      ad27cc914ae10ffa3f7d.js
      963d5b9a3244b784cf68.js
      1a928bdf5e64d32dfb79.js
      a2ea8cc6f932ed3a82f5.js
      40efb8c8624f9a9e643a.js
      711ccfbcc1756e291b92.js
      098c16dd9b6afa7df2ea.js
      eca7cbd1ba1dbd56c760.js
      1e5e9977d6d9ff6e159e.js
      a33bbb4430d2064047f7.js
      d75f116e192f6f03d594.js
      96a932091545b76b5e86.js
      6f471d2c3e882b5cd019.js
      8ae201451e029fe6d06b.js
      519cb3e1974a60802da3.js
      bab67c7847fc9663afcb.js
      f899108d59d672a4144b.js
      08fe6ccc85bde5442928.js
      5b14d518481bf7941a5f.js
      bc78cc76f9d9bad420cf.js
      b65085645de1741e34a1.js
      382b6616b1922d8c3fa3.js
      695ec68a02a2f24da09b.js
      51d77c8bd01fe61e7976.js
      98f582214b1012cf335e.js
      0c5315ca59b6a4b11998.js
      a2cdd6f6383ce51bacbc.js
      31f4404d95760d73a444.js
      5110e5c8e973efbe194c.js
      26ef8710b538c1d44236.js
      40cb21b2dbf0d0361a07.js
      680779d533b8c35404e0.js
      c004389fc798cee77e88.js
      8037e13a6bc4ef474928.js
      7cba2402304d44fd2f79.js
      2b7b7c6a93886b77a86e.js
      fb7deb6061d165bbe83c.js
      1ce220de750fabc5160f.js
      62b72657b295087712c0.js
      c89199cc3a0b8edbf357.js
      f95026ecaa4c96daa665.js
      896d08bc28d287f87b9d.js
      72fb43604a8752462afc.js
      7b94a62ab90f988d0ffb.js
      eb3bb168d6509ea10c07.js
      6efc3866ac34e22769a1.js
      93e18a0dbd824cab7557.js
      627c083918abe80d9d2b.js
      e1f8543bafb6b0de4405.js
      53c78568edb849c17fe1.js
      c401c2accd656f2caf9f.js
      af1754adceeb6594a482.js
      622b55b8ddc7ec0b0b1b.js
      92b860f7366f6a7b74a3.js
      bb050b0a252816ce452f.js
      fb05d20074e0533140b6.js
      d2a86154161533d3ca98.js
      4ae4bc2010679a6f8a06.js
      7f0b056a02904b370853.js
      6fba427b197e7704e21b.js
      b0b66b1513ae5293e32e.js
      d405ce3040179ea8c4d9.js
      20beab345541c9242fe9.js
      137c6b61233d550e39da.js
      03f64b7932ddb984aece.js
      26efc6bee4c2dccbbd81.js
      3fadce39d26d97f5e182.js
    * b5b5c2cabb502d373c3c.js
      ea52810b45995d816ba8.js
      90f14b8b62a9ba8b99d2.js
      7b3a07d6c33e20d18a00.js
      3832aa4ec0c9c26b4f46.js
      fd391e782ffd29c7cc17.js
    
    Stylesheets (1):
    * 614a3d7b1d364d908d57.css

--- data/client/channels/ptb/scripts/scripts/c401c2accd656f2caf9f.js
+++ data/client/channels/ptb/scripts/scripts/c401c2accd656f2caf9f.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "37b55f8d77e7ec56a87d49261ab9b7c60bc91b83"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "f903260a28936cc595e92e685e333c2970d57d03"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
@@ -1160,4 +1160,4 @@
         }
     }
 ]);
-//# sourceMappingURL=9e98d50069db6df11146.js.map
+//# sourceMappingURL=c401c2accd656f2caf9f.js.map
--- data/client/channels/ptb/scripts/scripts/d2a86154161533d3ca98.js
+++ data/client/channels/ptb/scripts/scripts/d2a86154161533d3ca98.js
@@ -48614,7 +48614,7 @@
                         var i;
                         let d = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "240437"
+                                build_number: "240467"
                             },
                             _ = o.default.getCurrentUser();
                         null != _ && (d.user_id = _.id, d.user_name = _.tag, null != _.email && (d.email = _.email));
@@ -51559,4 +51559,4 @@
         }
     }
 ]);
-//# sourceMappingURL=4dc56c6a9ecdc94de786.js.map
+//# sourceMappingURL=d2a86154161533d3ca98.js.map
