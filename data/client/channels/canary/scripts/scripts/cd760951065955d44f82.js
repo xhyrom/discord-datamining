f59d2fc6462f41e624e7add4410d14071dc11aa2
xHyroM <github@xhyrom.dev>
2023-10-26 21:52:59 +0000

    📥 Canary Build 240856 (21fdacf16d1d617809d0a40d22d60caf2b1ab8a7)
    Build Number: 240856
    Version Hash: 21fdacf16d1d617809d0a40d22d60caf2b1ab8a7
    Host Version: 1.0.85
    Build At: 26/9/2023 21:42:48 (1698356568984)
    
    Scripts (103):
      c252b5fa872ccc680f3e.js
      68ac062a2160f436183f.js
      29bda2d7fef4a4f5b599.js
      2740ede9403c7526f84d.js
      79c892edd26d34740925.js
      0b2d3b11fe89441e9a68.js
      3d86c5d7d4a7d10dfd01.js
      94e9e8b3bbf30f475165.js
      4084926f980976ae3509.js
      acaf34c8ae2e6364014a.js
      6754459bfa4b06a0ce42.js
      2ad9b9a2a424b7fb294d.js
      80d55722485c0c45c3f6.js
      140339c75158017cbd89.js
      1bcd78fc1311157eceeb.js
      6f6aa3f097dd5dd2633e.js
      9a87c6d4fc22807915fc.js
      28cbbf53e79cf7167475.js
      cd760951065955d44f82.js
      7f335bf966ae4beb1fdb.js
      3a68b3916ecf07a1a230.js
      7f19cfd25c2efa0c0e67.js
      8dab3d9dcd3c3a30eb3b.js
      fe753e272fca732721e3.js
      16a129af7446703c7922.js
      3bb34a166eaaa8d5569c.js
      fef447fb8a823fc0697b.js
      9542ad1ed6d0bccb3cb8.js
      99edae77060ac81239dd.js
      33d6aeac832dce305350.js
      ad27cc914ae10ffa3f7d.js
      963d5b9a3244b784cf68.js
      1a928bdf5e64d32dfb79.js
      a2ea8cc6f932ed3a82f5.js
      40efb8c8624f9a9e643a.js
      711ccfbcc1756e291b92.js
      098c16dd9b6afa7df2ea.js
      eca7cbd1ba1dbd56c760.js
      1e5e9977d6d9ff6e159e.js
      a33bbb4430d2064047f7.js
      688d68e04301a4cacfce.js
      96a932091545b76b5e86.js
      6f471d2c3e882b5cd019.js
      8ae201451e029fe6d06b.js
      519cb3e1974a60802da3.js
      bab67c7847fc9663afcb.js
      f899108d59d672a4144b.js
      08fe6ccc85bde5442928.js
      5b14d518481bf7941a5f.js
      bc78cc76f9d9bad420cf.js
      b65085645de1741e34a1.js
      382b6616b1922d8c3fa3.js
      695ec68a02a2f24da09b.js
      51d77c8bd01fe61e7976.js
      98f582214b1012cf335e.js
      0c5315ca59b6a4b11998.js
      a2cdd6f6383ce51bacbc.js
      31f4404d95760d73a444.js
      5110e5c8e973efbe194c.js
      26ef8710b538c1d44236.js
      40cb21b2dbf0d0361a07.js
      680779d533b8c35404e0.js
      8f88475d9faacc1a1dd0.js
      8037e13a6bc4ef474928.js
      7cba2402304d44fd2f79.js
      2b7b7c6a93886b77a86e.js
      fb7deb6061d165bbe83c.js
      1ce220de750fabc5160f.js
      62b72657b295087712c0.js
      1a5e27f77039b31f3c05.js
      2f64583310103ffccb34.js
      b5428e687cd6b15c9322.js
      72fb43604a8752462afc.js
      7b94a62ab90f988d0ffb.js
      eb3bb168d6509ea10c07.js
      6efc3866ac34e22769a1.js
      93e18a0dbd824cab7557.js
      627c083918abe80d9d2b.js
      e1f8543bafb6b0de4405.js
      53c78568edb849c17fe1.js
      58b7649faf48d4300ecd.js
      af1754adceeb6594a482.js
      622b55b8ddc7ec0b0b1b.js
      92b860f7366f6a7b74a3.js
      c495fed61aa77467376d.js
      fb05d20074e0533140b6.js
      e8e1462fb8fa6a42dbf3.js
      9dba2f8f1ec8e4087c9c.js
      7f0b056a02904b370853.js
      6fba427b197e7704e21b.js
      b0b66b1513ae5293e32e.js
      04ba7d07603ab130c5f7.js
      20beab345541c9242fe9.js
      137c6b61233d550e39da.js
      03f64b7932ddb984aece.js
      26efc6bee4c2dccbbd81.js
      dd826b74f60e2f229b34.js
    * 8b574d497d9fd455cba1.js
      ea52810b45995d816ba8.js
      90f14b8b62a9ba8b99d2.js
      48bc47123d50b952165e.js
      8cf7bdb2a3ee694bcf57.js
      b91c39169eb6e0481b6a.js
    
    Stylesheets (1):
    * c755576cdcfcc17be13c.css

--- data/client/channels/canary/scripts/scripts/cd760951065955d44f82.js
+++ data/client/channels/canary/scripts/scripts/cd760951065955d44f82.js
@@ -9876,7 +9876,7 @@
                 let r = {},
                     i = window.GLOBAL_ENV.RELEASE_CHANNEL;
                 i && (r.release_channel = i.split("-")[0]);
-                let o = parseInt((n = "240848", "240848"), 10);
+                let o = parseInt((n = "240856", "240856"), 10);
                 !isNaN(o) && (r.client_build_number = o);
                 let a = null == S ? void 0 : null === (e = (t = S.remoteApp).getBuildNumber) || void 0 === e ? void 0 : e.call(t);
                 return !isNaN(a) && (r.native_build_number = a), r.client_event_source = function() {
@@ -10118,4 +10118,4 @@
         }
     }
 ]);
-//# sourceMappingURL=20e2dc75c06ff583ac7e.js.map
+//# sourceMappingURL=cd760951065955d44f82.js.map
