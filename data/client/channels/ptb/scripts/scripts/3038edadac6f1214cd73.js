54d6d8c847c6dd29e6c73b8777c400cdc25b7d2a
xHyroM <github@xhyrom.dev>
2023-10-28 00:34:19 +0000

    📥 Public Testing Beta Build 241450 (1b6cba8e7db4ef652caca552ab5900f680bad96d)
    Build Number: 241450
    Version Hash: 1b6cba8e7db4ef652caca552ab5900f680bad96d
    Host Version: 1.0.1040
    Build At: 28/9/2023 0:3:34 (1698451414159)
    
    Scripts (103):
      97126000f7a2c8502c2d.js
      68ac062a2160f436183f.js
      29bda2d7fef4a4f5b599.js
      2740ede9403c7526f84d.js
      79c892edd26d34740925.js
      0b2d3b11fe89441e9a68.js
      3d86c5d7d4a7d10dfd01.js
      94e9e8b3bbf30f475165.js
      4084926f980976ae3509.js
      acaf34c8ae2e6364014a.js
      6754459bfa4b06a0ce42.js
      2ad9b9a2a424b7fb294d.js
      80d55722485c0c45c3f6.js
      140339c75158017cbd89.js
      1bcd78fc1311157eceeb.js
      6f6aa3f097dd5dd2633e.js
      9a87c6d4fc22807915fc.js
      fe3b4f1937cccea04aec.js
      332ca237d5f531b385a8.js
      7f335bf966ae4beb1fdb.js
      3a68b3916ecf07a1a230.js
      7f19cfd25c2efa0c0e67.js
      0eef4a09d323dd69dc54.js
      fe753e272fca732721e3.js
      16a129af7446703c7922.js
      3bb34a166eaaa8d5569c.js
      fd28d9cb45cef90bb628.js
      d467b1f1a2d58a169b6b.js
      551be2d2f476c7584c3a.js
      33d6aeac832dce305350.js
      ad27cc914ae10ffa3f7d.js
      963d5b9a3244b784cf68.js
      1a928bdf5e64d32dfb79.js
      a2ea8cc6f932ed3a82f5.js
      40efb8c8624f9a9e643a.js
      711ccfbcc1756e291b92.js
      098c16dd9b6afa7df2ea.js
      a56b7b6c083da0597b44.js
      1e5e9977d6d9ff6e159e.js
      a33bbb4430d2064047f7.js
      cebc7228b7e290fd0c40.js
      96a932091545b76b5e86.js
      6f471d2c3e882b5cd019.js
      519cb3e1974a60802da3.js
      8ae201451e029fe6d06b.js
      e1fa3e91ec551f52afab.js
      f71af37f72940995baf3.js
      08fe6ccc85bde5442928.js
      5b14d518481bf7941a5f.js
      bc78cc76f9d9bad420cf.js
      b65085645de1741e34a1.js
      382b6616b1922d8c3fa3.js
      695ec68a02a2f24da09b.js
      51d77c8bd01fe61e7976.js
      017c10647bfc03f3dda3.js
      0c5315ca59b6a4b11998.js
      7d8da229e0177bbbb531.js
      31f4404d95760d73a444.js
      5110e5c8e973efbe194c.js
      26ef8710b538c1d44236.js
      40cb21b2dbf0d0361a07.js
      680779d533b8c35404e0.js
      8f88475d9faacc1a1dd0.js
      8037e13a6bc4ef474928.js
      7cba2402304d44fd2f79.js
      2b7b7c6a93886b77a86e.js
      1ce220de750fabc5160f.js
      fb7deb6061d165bbe83c.js
      62b72657b295087712c0.js
      df526ce1294c6fb1a68e.js
      2f64583310103ffccb34.js
      36bba5fbfab8f10e3b3b.js
      72fb43604a8752462afc.js
      7b94a62ab90f988d0ffb.js
      eb3bb168d6509ea10c07.js
      6efc3866ac34e22769a1.js
      93e18a0dbd824cab7557.js
      627c083918abe80d9d2b.js
      e1f8543bafb6b0de4405.js
      53c78568edb849c17fe1.js
      af1754adceeb6594a482.js
      36ea748f734d3c350f63.js
      622b55b8ddc7ec0b0b1b.js
      92b860f7366f6a7b74a3.js
      c495fed61aa77467376d.js
      fb05d20074e0533140b6.js
      660b55c057f34b56f8a3.js
      9dba2f8f1ec8e4087c9c.js
      7f0b056a02904b370853.js
      6fba427b197e7704e21b.js
      ec87686f81cc7b820911.js
      04ba7d07603ab130c5f7.js
      20beab345541c9242fe9.js
      137c6b61233d550e39da.js
      03f64b7932ddb984aece.js
      26efc6bee4c2dccbbd81.js
      dd826b74f60e2f229b34.js
    * 12348ba8cb00c278e17a.js
      ea52810b45995d816ba8.js
      90f14b8b62a9ba8b99d2.js
      81a1ff6331f459deaea5.js
      3038edadac6f1214cd73.js
      2942ff413db9b72325d3.js
    
    Stylesheets (1):
    * f734f7fcc7adefdda7c8.css

--- data/client/channels/ptb/scripts/scripts/3038edadac6f1214cd73.js
+++ data/client/channels/ptb/scripts/scripts/3038edadac6f1214cd73.js
@@ -17788,7 +17788,7 @@
                 L = E("782340");
             (0, r.setUpdateRules)(i.default), (0, I.default)(L.default, n.default, s.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
             let u = window.GLOBAL_ENV.RELEASE_CHANNEL;
-            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("241445", ", Version Hash: ").concat("c602e0eefb166b5c60be3e14e28a73ab9e7ef54e")), t.default.setTags({
+            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("241450", ", Version Hash: ").concat("1b6cba8e7db4ef652caca552ab5900f680bad96d")), t.default.setTags({
                 appContext: l.CURRENT_APP_CONTEXT
             }), S.default.initBasic(), N.default.init(), T.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
         },
@@ -20150,8 +20150,8 @@
 
             function o() {
                 var e;
-                let _ = parseInt((e = "241445", "241445"));
-                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("241445")), _ = 0), _
+                let _ = parseInt((e = "241450", "241450"));
+                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("241450")), _ = 0), _
             }
         },
         990629: function(e, _, E) {
@@ -29408,4 +29408,4 @@
         }
     }
 ]);
-//# sourceMappingURL=94ab5e75ca162a0e3445.js.map
+//# sourceMappingURL=3038edadac6f1214cd73.js.map
--- data/client/channels/ptb/scripts/scripts/36ea748f734d3c350f63.js
+++ data/client/channels/ptb/scripts/scripts/36ea748f734d3c350f63.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "c602e0eefb166b5c60be3e14e28a73ab9e7ef54e"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "1b6cba8e7db4ef652caca552ab5900f680bad96d"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
@@ -1160,4 +1160,4 @@
         }
     }
 ]);
-//# sourceMappingURL=45a9ca86a45dd87555c6.js.map
+//# sourceMappingURL=36ea748f734d3c350f63.js.map
