b1ac818eddc6b89c52d39b99ef3230e1b481ffe4
xHyroM <github@xhyrom.dev>
2023-10-31 22:01:11 +0000

    📥 Canary Build 242249 (91f2cf8c5c1e7bd9bdb5d81faab4b9c4f801ac39)
    Build Number: 242249
    Version Hash: 91f2cf8c5c1e7bd9bdb5d81faab4b9c4f801ac39
    Host Version: 1.0.90
    Build At: 31/9/2023 21:55:19 (1698789319964)
    
    Scripts (100):
      e8662f016f34c83df1b5.js
      cb5a644e06978c11b1b1.js
      5e5eb0033880c66ceed0.js
      6d248d09aa3e0db2a279.js
      9c60ed5f2d847cd6e59f.js
      0bef69d5f1c6fa6e68cf.js
      266149bb60015a51c550.js
      d2b2505c069c21013d2b.js
      e39b2ad738cd0f3eea70.js
      832f099b5c9f538b024d.js
      0b013f5248a9685aab50.js
      ba4019a34c1ac76aa0f1.js
      476939f96d701139922d.js
      2a308b3c8ad7d996bb6f.js
      9cd8448574ccf479b36b.js
      fcfa42394503368b920f.js
      0cbd3f62b315bf33697e.js
      21c0396d7861560c4ee9.js
      5819875dc2a2f1025d4b.js
      762e6787f099dff585c4.js
      734c3f9b5a6d6a93bb8e.js
      1dc08b22ce71744308af.js
      d140d7c5b1df019ce58b.js
      0d4d204f61448daba03a.js
      f4ec837d1ba77887ad94.js
      c7aaed1e38be2027060d.js
      9af5ca8dd07a6b494bfd.js
      86f806af7c2ed02cb4e4.js
      ff41becf530cc2172b3e.js
      e73d4af8f8d0fb2da7dc.js
      100d5e66ecf33a783fe4.js
      9499aa6abb8f7b3666b2.js
      fc894f21cdfece5ca281.js
      fb2f0753465381a2a511.js
      ab5ae66f83934ac09063.js
      3b411ddeae4ee523fd14.js
      766c9c0407630d6270e5.js
      f03ed4c81b719b138431.js
      99ce1195e0a9ed2dfb3f.js
      5f67777e6b2178bdeb4b.js
      4504bec69c727bc377ff.js
      2d9b39bc81bf3619dc27.js
      2a6212b6f72bf768d413.js
      e4f92eddfe3a4ef68f1a.js
      879ae7c791403a180239.js
      cffcc71416d450865e94.js
      70690447b0a9a479f9ee.js
      8434d3712c3b5cc68dcf.js
      a079e993c8a728a3b00b.js
      695ec68a02a2f24da09b.js
      79e6c46f8fae8c8c2d7a.js
      46014ba1b1a3f3843793.js
      ff1cd81cb4aadea08ef8.js
      d1d386c979f8d71656be.js
      68f53e9de6e51f3c7f7f.js
      74db89596810859efe09.js
      1279d5f1796bdc03dd04.js
      d82268f01603c4109b3b.js
      0c2874b4fa905d6d4657.js
      c406fa9df8761f79d2d1.js
      a3892550d233eb36a0ef.js
      803198c0c213cf62b4aa.js
      0a5e2a97276ac789aaad.js
      5d06adfef69586a9269a.js
      2ba3cc3d8275c495b9be.js
      fab1133c7d4070677fcb.js
      f061e2e8f63764c32c77.js
      de87dd2d7df0b80d91da.js
      f808f0d0fd4d8ae50090.js
      e5526701a7c859e07dae.js
      b5304b5a0dd24ad8e6fc.js
      afb8048bf11e456aea78.js
      857f367e1049c4cfb9b5.js
      aa92103430877ff6fddb.js
      cfea62243daa1fb6cd64.js
      32c5bea01efe86e74ddf.js
      9b246fb0cc6df15b3b85.js
      7dcda81bfba0231b0651.js
      e1bd3dee9ed775e1193f.js
      fbb5e45afd3dff18782a.js
      b7a17ae0d0486a4eb097.js
      a3e7272e4aa762f0cf74.js
      40a3b138bd5c9a71ba6b.js
      7ddcf42dfedaf54393a8.js
      1972600b5d7f1a276e8c.js
      30a77419a47045448938.js
      af942a733c169126d59d.js
      2963543fdad76d09a26e.js
      8451055e8d6dec746a41.js
      eea54c589a3229844858.js
      fed0b45b21280f4bc95e.js
      95b722196a6e236e6da1.js
      6136e51056d876832431.js
      a101791cd927477c634c.js
    * 2e1fd6c1d7e9d8eaf1be.js
      9e554ca167451fffc5dd.js
      678db20dfe131728bc51.js
      049c79ec47ba9f23bcc4.js
      32153ca811199d782328.js
      f8243aac9bca6d3e62fb.js
    
    Stylesheets (1):
    * 889b96239b8a557064fb.css

--- data/client/channels/canary/scripts/scripts/9b246fb0cc6df15b3b85.js
+++ data/client/channels/canary/scripts/scripts/9b246fb0cc6df15b3b85.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "2d70a9381a17672a8ae476abf76a198cfa4111b1"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "91f2cf8c5c1e7bd9bdb5d81faab4b9c4f801ac39"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
@@ -1175,4 +1175,4 @@
         }
     }
 ]);
-//# sourceMappingURL=8a7706b010d0adde1300.js.map
+//# sourceMappingURL=9b246fb0cc6df15b3b85.js.map
