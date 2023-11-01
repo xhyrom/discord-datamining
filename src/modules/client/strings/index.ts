633e61a30bb8c8a97923e6de53bed172fc450eab
Jozef Steinhübl <generalkubo@gmail.com>
2023-11-01 13:03:31 +0100

    Update index.ts

--- src/modules/client/strings/index.ts
+++ src/modules/client/strings/index.ts
@@ -46,7 +46,7 @@ export class Strings implements Module {
 
     const strings = getLangStrings(
       (await readFile(
-        join(this.baseDir, "channels", "canary", "scripts", "strings.js")
+        join(this.baseDir, "channels", "canary", "web", "scripts", "strings.js")
       )) ?? ""
     );
 
