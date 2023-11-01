9883a735ff32294dd0f319fb01978f868f513e5f
xHyroM <github@xhyrom.dev>
2023-11-01 18:57:45 +0000

    📥 Public Testing Beta Web Build 242560 (b94accf961ff2fdf20956b182d59ff970d9a6897)
    Build Number: 242560
    Version Hash: b94accf961ff2fdf20956b182d59ff970d9a6897
    Build At: 1/10/2023 18:50:36 (1698864636909)
    
    Scripts (94):
      d5ab42ada64dc6dabd42.js
      33fb93a6ef74303162e5.js
      6d248d09aa3e0db2a279.js
      bb76a6474f113f56fd33.js
      5e5eb0033880c66ceed0.js
      9c60ed5f2d847cd6e59f.js
      0bef69d5f1c6fa6e68cf.js
      266149bb60015a51c550.js
      d2b2505c069c21013d2b.js
      e39b2ad738cd0f3eea70.js
      832f099b5c9f538b024d.js
      0b013f5248a9685aab50.js
      476939f96d701139922d.js
      2a308b3c8ad7d996bb6f.js
      9cd8448574ccf479b36b.js
      666dbe3db5cd113ef625.js
      0632859de3f4790fc5ff.js
      b2da55d0f550850e9ab2.js
      5b9c5ef44a35660974fb.js
      762e6787f099dff585c4.js
      b6e0d3ceb60386ad702d.js
      f7b107283ef5668e59e9.js
      ab5ae66f83934ac09063.js
      fa5f082b6c68e398d20d.js
      f609cf708dc4e463cc3c.js
      b0d371fc397e7b7b9a4a.js
      9c1031e2598383265675.js
      f4ec837d1ba77887ad94.js
      c7aaed1e38be2027060d.js
      df9c1357bd32e8a7f4dd.js
      86f806af7c2ed02cb4e4.js
      100d5e66ecf33a783fe4.js
      fc894f21cdfece5ca281.js
      fb2f0753465381a2a511.js
      34cc6e514c62622a2590.js
      d88cebc08b6be28647d7.js
      05c198166a6b917fe733.js
      ffb80ee8804ff51d1d66.js
      2ce217de6147359e9cd0.js
      e9a351b295e82a50b02f.js
      3c307eeb894e267c4b2b.js
      2a6212b6f72bf768d413.js
      879ae7c791403a180239.js
      70690447b0a9a479f9ee.js
      8434d3712c3b5cc68dcf.js
      79e6c46f8fae8c8c2d7a.js
      46014ba1b1a3f3843793.js
      6dca3c6f7bf34eafa9fd.js
      d1d386c979f8d71656be.js
      993df709ed5483e8e7e0.js
      1279d5f1796bdc03dd04.js
      d82268f01603c4109b3b.js
      c92f95b719cb6cbaddb7.js
      7cbc744d43e1e164afd9.js
      f8d17df9fa6b0a2b7f02.js
      3d7d7093dcdb6e6b58a5.js
      a3892550d233eb36a0ef.js
      1642fd8a8bd315f0ce60.js
      053dba7559cb20c5c203.js
      5d06adfef69586a9269a.js
      2ba3cc3d8275c495b9be.js
      9e6691a17b995d749083.js
      3972b4d4a854235f9af2.js
      205b387f3338361f468e.js
      e5526701a7c859e07dae.js
      b5304b5a0dd24ad8e6fc.js
      e4197a9e24b8955069ef.js
      058a835c74d6bb0fba7a.js
      a079e993c8a728a3b00b.js
      aa92103430877ff6fddb.js
      b691a5e9879a425b5856.js
      32c5bea01efe86e74ddf.js
      ca7345301beec450f40a.js
      7dcda81bfba0231b0651.js
      1a6ab9caf152c49d8dca.js
      fbb5e45afd3dff18782a.js
      b7a17ae0d0486a4eb097.js
      1803f36649b9e0d8a5a2.js
      41a27f9f30022bd0a645.js
      1972600b5d7f1a276e8c.js
      30a77419a47045448938.js
      f9c50046e0dbf97ad563.js
      8451055e8d6dec746a41.js
      b8afb0fb11d2adb75c57.js
      eea54c589a3229844858.js
      95b722196a6e236e6da1.js
      6136e51056d876832431.js
    * cf2b4e814e9dc728e33a.js
      eda3e0522a69a006b0d0.js
      649ace1946baf4b1fd96.js
      857026a4ec82bcc0fb87.js
      ce79aa9c2eaf8dabda24.js
      3beea98d9926bb5e8a5e.js
      5fcebdec4091f45b862c.js
    
    Stylesheets (2):
      shared.60ef38d7083456bd51d2.css
    * app.9ccaa1cb6b9606624f8e.css

--- data/client/channels/ptb/web/scripts/scripts/1803f36649b9e0d8a5a2.js
+++ data/client/channels/ptb/web/scripts/scripts/1803f36649b9e0d8a5a2.js
@@ -33644,7 +33644,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "242543"
+                                build_number: "242560"
                             },
                             E = l.default.getCurrentUser();
                         null != E && (c.user_id = E.id, c.user_name = E.tag, null != E.email && (c.email = E.email));
@@ -46306,4 +46306,4 @@
         }
     }
 ]);
-//# sourceMappingURL=1b4c352b6612cae984a7.js.map
+//# sourceMappingURL=1803f36649b9e0d8a5a2.js.map
--- data/client/channels/ptb/web/scripts/scripts/41a27f9f30022bd0a645.js
+++ data/client/channels/ptb/web/scripts/scripts/41a27f9f30022bd0a645.js
@@ -2066,7 +2066,7 @@
                 let i = {},
                     r = window.GLOBAL_ENV.RELEASE_CHANNEL;
                 r && (i.release_channel = r.split("-")[0]);
-                let o = parseInt((n = "242543", "242543"), 10);
+                let o = parseInt((n = "242560", "242560"), 10);
                 !isNaN(o) && (i.client_build_number = o);
                 let s = null == C ? void 0 : null === (e = (t = C.remoteApp).getBuildNumber) || void 0 === e ? void 0 : e.call(t);
                 return !isNaN(s) && (i.native_build_number = s), i.client_event_source = function() {
@@ -2308,4 +2308,4 @@
         }
     }
 ]);
-//# sourceMappingURL=ac90a026d2d559fd3195.js.map
+//# sourceMappingURL=41a27f9f30022bd0a645.js.map
