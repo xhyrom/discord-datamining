90e20d69352dd9d024e9a1084ab1b324a83d78cb
xHyroM <github@xhyrom.dev>
2023-11-03 17:40:18 +0000

    📥 Public Testing Beta Web Build 243499 (fa981d2f24c3ad8773d03b7c2bbbe4d028582e20)
    Build Number: 243499
    Version Hash: fa981d2f24c3ad8773d03b7c2bbbe4d028582e20
    Build At: 3/10/2023 17:33:26 (1699032806980)
    
    Scripts (91):
      0fdf6ea5e6df62acd046.js
      969a8ec4f1fe9a5f7468.js
      f53fb8dc830e89b4f8ec.js
      96e3c27f22f4e531c022.js
      0bef69d5f1c6fa6e68cf.js
      266149bb60015a51c550.js
      cd59045b785bc75d0617.js
      832f099b5c9f538b024d.js
      0b013f5248a9685aab50.js
      db8ee73db79786fd3a89.js
      476939f96d701139922d.js
      2a308b3c8ad7d996bb6f.js
      9cd8448574ccf479b36b.js
      fcfa42394503368b920f.js
      0cbd3f62b315bf33697e.js
      21c0396d7861560c4ee9.js
      762e6787f099dff585c4.js
      220bf23d249c6e2271cc.js
      00bf6291f70b6742f944.js
      1dc08b22ce71744308af.js
      82c816e241ac31d29c34.js
      2cc86cdbc77ea5c755d9.js
      5a26b1807e3353a83f06.js
      f4ec837d1ba77887ad94.js
      9af5ca8dd07a6b494bfd.js
      86f806af7c2ed02cb4e4.js
      ff41becf530cc2172b3e.js
      c396ed669bb61a0e768d.js
      9499aa6abb8f7b3666b2.js
      fc894f21cdfece5ca281.js
      fb2f0753465381a2a511.js
      b2d59c807a4160e2b604.js
      b65d469248c5bb6addad.js
      74db89596810859efe09.js
      c6b2dc55720b7a85e2ac.js
      4504bec69c727bc377ff.js
      2a6212b6f72bf768d413.js
      e4f92eddfe3a4ef68f1a.js
      547b43ccf35dc8adc7b4.js
      b2171d350a6fc181e8ed.js
      70690447b0a9a479f9ee.js
      8434d3712c3b5cc68dcf.js
      7642c3ed74fa65b8c321.js
      578e8029123c2c4025de.js
      b9bd6cd01d1ba865865c.js
      a21cf9cf571830e70f5a.js
      8c2a14e40dfa342a5eab.js
      d82268f01603c4109b3b.js
      5d47c82e6f63c3c47c06.js
      11eec8468d4986cfd908.js
      6311bfd1faaf1bc454e2.js
      803198c0c213cf62b4aa.js
      a3892550d233eb36a0ef.js
      9ca450e010405ce9659c.js
      5d06adfef69586a9269a.js
      2ba3cc3d8275c495b9be.js
      7fc1a6b5e64073ca8623.js
      3496b87461057ba323cf.js
      3ce551bd730c48731158.js
      b5304b5a0dd24ad8e6fc.js
      afb8048bf11e456aea78.js
      20d8baaccac5b15e47f6.js
      a079e993c8a728a3b00b.js
      9a4ff8105484c42091e2.js
      cfea62243daa1fb6cd64.js
      ca717fa7b0dcd9c09904.js
      5ca3059120c966a51bd4.js
      7dcda81bfba0231b0651.js
      e1bd3dee9ed775e1193f.js
      fbb5e45afd3dff18782a.js
      b7a17ae0d0486a4eb097.js
      a3e7272e4aa762f0cf74.js
      3b7d66bbfc1d0824720b.js
      8c8665927942b85427a4.js
      1972600b5d7f1a276e8c.js
      2169ecdf4d8443f86a34.js
      caeef036f0243ab4866c.js
    * 30ddaffd76531ed6956e.js
      af942a733c169126d59d.js
      2963543fdad76d09a26e.js
      eea54c589a3229844858.js
      95b722196a6e236e6da1.js
      676f61c024f980375c18.js
      6136e51056d876832431.js
      6440ce6db8a28d82581d.js
      8a2e5e54bf63e1471e60.js
      7503e91913f2b3242cc3.js
      678db20dfe131728bc51.js
      280712a1c9411033f1e4.js
      867ef6319c225367d96d.js
      93dea0b456b247c81263.js
    
    Stylesheets (2):
      shared.82f8b23654596345ff7e.css
    * app.906137cd435f87ac636f.css

--- data/client/channels/ptb/web/scripts/main.js
+++ data/client/channels/ptb/web/scripts/main.js
@@ -1751,7 +1751,7 @@
                     y.current.cancel(), N(!0)
                 }, []), x = r.useCallback(() => {
                     y.current.delay()
-                }, []), P = r.useCallback((e, t) => {
+                }, []), b = r.useCallback((e, t) => {
                     R(), (0, f.openContextMenuLazy)(e, async () => {
                         let {
                             default: e
@@ -1773,13 +1773,13 @@
                     className: g,
                     participantType: s.type
                 });
-                let b = o(D).take(v).map(e => (0, a.jsx)(c.Avatar, {
+                let P = o(D).take(v).map(e => (0, a.jsx)(c.Avatar, {
                     src: e.getAvatarURL(l, 24),
                     "aria-label": e.username,
                     size: c.AvatarSizes.SIZE_24,
                     className: I.viewer
                 }, e.id)).value();
-                return D.length > v && (b[b.length - 1] = (0, a.jsxs)("div", {
+                return D.length > v && (P[P.length - 1] = (0, a.jsxs)("div", {
                     className: I.overflow,
                     children: ["+", D.length - v + 1]
                 }, "overflow")), (0, a.jsx)(p.default, {
@@ -1790,7 +1790,7 @@
                         children: (0, a.jsx)(c.Popout, {
                             renderPopout: () => (0, a.jsx)(M, {
                                 participantType: s.type,
-                                handleUserContextMenu: P,
+                                handleUserContextMenu: b,
                                 guildId: l,
                                 channelId: t,
                                 users: D,
@@ -1800,7 +1800,7 @@
                             position: "top",
                             children: () => (0, a.jsx)("div", {
                                 className: i(I.viewers, g),
-                                children: b
+                                children: P
                             })
                         })
                     })
@@ -2594,8 +2594,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "243495",
-                    versionHash: "cbd08e36dea1c19d23656b132f477d6dd1d77e3c"
+                    buildNumber: "243499",
+                    versionHash: "fa981d2f24c3ad8773d03b7c2bbbe4d028582e20"
                 }
             }
             n.r(t), n.d(t, {
@@ -2732,7 +2732,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-cbd08e36dea1c19d23656b132f477d6dd1d77e3c",
+                    release: "discord_web-fa981d2f24c3ad8773d03b7c2bbbe4d028582e20",
                     beforeSend: e => {
                         var t, n;
                         return !(null != (t = e).exception && null != t.exception.values && t.exception.values.every(e => null == e.stacktrace || null != e.stacktrace.frames && 1 === e.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || l.some(e => window.navigator.appVersion.toLowerCase().indexOf(e) >= 0)) && !i() && !("Aborted" === (n = e).message || "cancel captcha" === n.message) && s() ? e : null
@@ -2750,7 +2750,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), a.setTag("buildNumber", (e = "243495", "243495")), a.setTag("builtAt", String("1699032369894"));
+                }), a.setTag("buildNumber", (e = "243499", "243499")), a.setTag("builtAt", String("1699032806980"));
                 let t = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != t && "object" == typeof t)
                     for (let e in t) a.setTag(e, t[e]);
@@ -5173,4 +5173,4 @@
         }
     }
 ]);
-//# sourceMappingURL=5768231662c693b9a227.js.map
+//# sourceMappingURL=30ddaffd76531ed6956e.js.map
--- data/client/channels/ptb/web/scripts/scripts/8c8665927942b85427a4.js
+++ data/client/channels/ptb/web/scripts/scripts/8c8665927942b85427a4.js
@@ -5144,7 +5144,7 @@
                 let i = {},
                     r = window.GLOBAL_ENV.RELEASE_CHANNEL;
                 r && (i.release_channel = r.split("-")[0]);
-                let o = parseInt((n = "243495", "243495"), 10);
+                let o = parseInt((n = "243499", "243499"), 10);
                 !isNaN(o) && (i.client_build_number = o);
                 let s = null == N ? void 0 : null === (e = (t = N.remoteApp).getBuildNumber) || void 0 === e ? void 0 : e.call(t);
                 return !isNaN(s) && (i.native_build_number = s), i.client_event_source = function() {
@@ -5386,4 +5386,4 @@
         }
     }
 ]);
-//# sourceMappingURL=f8742f7ea839356403e5.js.map
+//# sourceMappingURL=8c8665927942b85427a4.js.map
