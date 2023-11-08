737c8317dbd8b1e088ef4f22f24e6127c5b6378f
xHyroM <github@xhyrom.dev>
2023-11-08 18:33:34 +0000

    📥 Canary Web Build 244468 (25945ad0382ce42f9f505afaaa62a9ffe8b6d644)
    Build Number: 244468
    Version Hash: 25945ad0382ce42f9f505afaaa62a9ffe8b6d644
    Build At: 8/10/2023 18:25:49 (1699467949119)
    
    Scripts (95):
      ff60668cd7b08bb05853.js
      6879fd5bea7716fb1369.js
      dd6cb5301e9e6be15613.js
      e92bcd49a35176c74689.js
      275913a14b80649f7304.js
      4f809fef37683ac514f1.js
      316f08965c8d5a695356.js
      76af0343de3f62c22bfe.js
      d13babb482ba16a6291d.js
      fb2a112618b46c326e06.js
      411670360cf7d55ca7d3.js
      62144d4f6f9a0252fd3a.js
      7ebb95041b61a5bb04b8.js
      8008dff0e97e29f17606.js
      e1bef50f1ef5d3746294.js
      40fad28f01da56375e6a.js
      31eda5c529a4cc259c8b.js
      0062f062bf10fd4cb5c5.js
      987b3af0ef2f81f0f298.js
      0f4b7cb7ecd30499211e.js
      014018262854fc32dd0a.js
      9dd89ec9674ca38c2b9c.js
      1acbba7c36d7919dfc8c.js
      b3d9afb1de73361444d9.js
      f4e4a31f10efba53eaf1.js
      6761fc5c6437483630f8.js
      6c1a73771fafed58293d.js
      c620cc89a59d6674e7fd.js
      345ae7315741c5558539.js
      db6756e9dd0f0bf2916f.js
      243e84ee5749691f53c4.js
      bc9d8985807d89e33a21.js
      9499aa6abb8f7b3666b2.js
      66b4de1639088065aad4.js
      fb2f0753465381a2a511.js
      1670b1e49a739a399f8d.js
      c6dd9ee8697421eacfa2.js
      ba60ecac374ac0077c25.js
      9a22220d68f36fd3a498.js
      a21c60eaaec66a269723.js
      7d097791abff786c8f1a.js
      e4f92eddfe3a4ef68f1a.js
      cededc02f40b299ba2d4.js
      b2171d350a6fc181e8ed.js
      02f7b829effcf627578b.js
      f71cc9f79f08bd3c3257.js
      7521cfcd66e380efa26b.js
      d7a10c52b99a3f419fb8.js
      6394d758f762b29b1394.js
      eeff8a13dd4f83df3f73.js
      744a0cbe303526abe5fb.js
      9489e0986a3d5256a438.js
      e82d8d0fc947a278e60f.js
      9abf73d7a99344de4247.js
      dba62d55e75eb96accc4.js
      22e5d3fb0e1c07f17064.js
      06ce2814448522654469.js
      2b92d5e872b9a5aff733.js
      ee430c4341b721db50a9.js
      eaa328fd6f6d6e902526.js
      0700368eddaecefa8434.js
      f6a94a7ab9c82eb6bbfd.js
      2fb3c6ae9568937ec862.js
      9401e22677bc2f7bc6d9.js
      350fbf8d55d8af3c42c2.js
      543fde11ae3afc1d15f0.js
      0d47d515b3149dc2b354.js
      9e77438d95cf54da592b.js
      da0edbe038c3ba57471a.js
      7f4dfa05381adb0d4950.js
      8c4d6bcdc2baa79061d8.js
      7dcda81bfba0231b0651.js
      42db9c78e759449d2599.js
      a829c071c1d8e3537520.js
      3068935444a8bbb4eac0.js
      f37cd230efa0b5df681f.js
      b54610e7ba942f354b7e.js
      34df79b5593e64d505a0.js
      1526f1d96ac7fddb72de.js
      863c8d5089a184979277.js
      35c9fe1fb3d2a86896e4.js
    * 2c4060c14610a83990fa.js
      dda9f6e73c3a38424c24.js
      e167d8278991717f799f.js
      736aab6e9f0251482a2a.js
      e52d67ae61ee5c3013dd.js
      57e3a42e83a7c52ad0f7.js
      ceea5be0b21ba3acab29.js
      409592685496f1680538.js
      472ef6caafde49a5fb43.js
      4c032704465e0d2e5726.js
      72d0a94857bda36fa6fb.js
      4c891112c170ba561ea7.js
      56cd6c94b57a0ae660ea.js
      0d5d9f104f023d715a3f.js
    
    Stylesheets (2):
      shared.d3617e67bf0cf2a4fdd9.css
    * app.d6c236a71b5ddf3d6750.css

--- data/client/channels/canary/web/scripts/scripts/2c4060c14610a83990fa.js
+++ data/client/channels/canary/web/scripts/scripts/2c4060c14610a83990fa.js
@@ -2594,8 +2594,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "244460",
-                    versionHash: "2629c28b4bdfbcdd303562ae4557078a5f5689cb"
+                    buildNumber: "244468",
+                    versionHash: "25945ad0382ce42f9f505afaaa62a9ffe8b6d644"
                 }
             }
             n.r(t), n.d(t, {
@@ -2732,7 +2732,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-2629c28b4bdfbcdd303562ae4557078a5f5689cb",
+                    release: "discord_web-25945ad0382ce42f9f505afaaa62a9ffe8b6d644",
                     beforeSend: e => {
                         var t, n;
                         return !(null != (t = e).exception && null != t.exception.values && t.exception.values.every(e => null == e.stacktrace || null != e.stacktrace.frames && 1 === e.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || l.some(e => window.navigator.appVersion.toLowerCase().indexOf(e) >= 0)) && !i() && !("Aborted" === (n = e).message || "cancel captcha" === n.message) && s() ? e : null
@@ -2750,7 +2750,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), a.setTag("buildNumber", (e = "244460", "244460")), a.setTag("builtAt", String("1699466990512"));
+                }), a.setTag("buildNumber", (e = "244468", "244468")), a.setTag("builtAt", String("1699467949119"));
                 let t = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != t && "object" == typeof t)
                     for (let e in t) a.setTag(e, t[e]);
@@ -5175,4 +5175,4 @@
         }
     }
 ]);
-//# sourceMappingURL=501913ad56eafc35f59b.js.map
+//# sourceMappingURL=2c4060c14610a83990fa.js.map
--- data/client/channels/canary/web/scripts/scripts/56cd6c94b57a0ae660ea.js
+++ data/client/channels/canary/web/scripts/scripts/56cd6c94b57a0ae660ea.js
@@ -18141,7 +18141,7 @@
                 u = E("782340");
             (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
             let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244460", ", Version Hash: ").concat("2629c28b4bdfbcdd303562ae4557078a5f5689cb")), t.default.setTags({
+            new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244468", ", Version Hash: ").concat("25945ad0382ce42f9f505afaaa62a9ffe8b6d644")), t.default.setTags({
                 appContext: l.CURRENT_APP_CONTEXT
             }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
         },
@@ -20530,8 +20530,8 @@
 
             function o() {
                 var e;
-                let _ = parseInt((e = "244460", "244460"));
-                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244460")), _ = 0), _
+                let _ = parseInt((e = "244468", "244468"));
+                return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244468")), _ = 0), _
             }
         },
         990629: function(e, _, E) {
@@ -36442,4 +36442,4 @@
         }
     }
 ]);
-//# sourceMappingURL=0a109b18cb0c00da4cbc.js.map
+//# sourceMappingURL=56cd6c94b57a0ae660ea.js.map
