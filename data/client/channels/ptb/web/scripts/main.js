c51b26b313eaaf0154ed5113a3ee561edd0b80c7
xHyroM <github@xhyrom.dev>
2023-11-09 02:30:31 +0000

    📥 Public Testing Beta Web Build 244692 (c18b7c86b363bc145469fdde34c5f393b77ec746)
    Build Number: 244692
    Version Hash: c18b7c86b363bc145469fdde34c5f393b77ec746
    Build At: 1/0/1970 0:0:0 (0)
    
    Scripts (96):
      c13ef44fec53bc7ec2b3.js
      6879fd5bea7716fb1369.js
      960550b4d192a0f9da50.js
      e92bcd49a35176c74689.js
      275913a14b80649f7304.js
      8add1db2a97e7a1b803a.js
      860941194c36a08ad18e.js
      e8cd1912d9ab669b5640.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      d179fa5fdde237bd188f.js
      8008dff0e97e29f17606.js
      7fc5dac90fa6ced3cf35.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      ee54f6ae7868dc25cdc5.js
      b3d9afb1de73361444d9.js
      549929154a253e35f47f.js
      eefc786dd2836b085546.js
      77c58eb0c94231b5b0c8.js
      3aa1f2257bf1a73eea6d.js
      e351b88b824dbec88711.js
      345ae7315741c5558539.js
      8d266d9fc7abe4d5a7e9.js
      97598f1cbc36bdd0e3e6.js
      1f70c6eb3686a39a7854.js
      9499aa6abb8f7b3666b2.js
      7e48766a0dead75fa5ae.js
      76ce3c1701b64d925597.js
      8b6cae3ad666328b0547.js
      5b5a4cc2e79476d1aaaf.js
      ba60ecac374ac0077c25.js
      9a22220d68f36fd3a498.js
      25dc10f313478679d26b.js
      21313f31f07ab7306ca3.js
      3ba791d5b2a97ee0a4ca.js
      b2171d350a6fc181e8ed.js
      370695f3dfaf71afc0e1.js
      f71cc9f79f08bd3c3257.js
      9221a252a9d51e007cca.js
      bc053c9af426cd215207.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      16fa93b0628537fca5c5.js
      bec9d4b88f67814ada9b.js
      e82d8d0fc947a278e60f.js
      a701f38853805ebc44fe.js
      5f43b0f52e3b53df9d39.js
      d05681f0492239d853d6.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      d3d33196ec4fb72a32e7.js
      7f43433916d6d96572c4.js
      7f8636ea24bd70dff157.js
      9a142a8f62b941139e72.js
      172de3b155f306b773be.js
      9401e22677bc2f7bc6d9.js
      cbee5cf1428e45590d8b.js
      7c519183a524bc6e1403.js
      206e13d6a40e4104e58d.js
      0d47d515b3149dc2b354.js
      6acda0f7011a6121b457.js
      9e77438d95cf54da592b.js
      6fd59308225556b7f410.js
      0a1c85aef610d6b88a03.js
      74e28e8dc215bf8e128b.js
      478ecc39ad990d3c0169.js
      a829c071c1d8e3537520.js
      587e3f4a405d796eda69.js
      11e0ae849b521f288679.js
      c49019f3dd2a9cdaa00f.js
      8fafb25c42d8bfd16967.js
      3229901073f7f5246652.js
      e5d3321c499c33bd0c5b.js
    * ab5fc7e6205d4458dd13.js
      05742b63f3a02e10537f.js
      f02e0fb527aecdbf40c0.js
      a1e0fbf8f9cbab9b443c.js
      a25c038ee9a959778ccc.js
      736aab6e9f0251482a2a.js
      84112e944bd8e0af2d10.js
      bc682dbd7e06417e3e0c.js
      ef1950fd673e1f073a9b.js
      a5da41c4e621479511cb.js
      f65dad1b4137c4f52dea.js
      e1e5cc7fe127e6611cad.js
      ba3f3f772756a218a429.js
      324ae9188e957837c77d.js
      251d2de60e93d575263e.js
    
    Stylesheets (2):
      shared.d3617e67bf0cf2a4fdd9.css
    * app.38f02b86cc12ce9c6f4c.css

--- data/client/channels/ptb/web/scripts/main.js
+++ data/client/channels/ptb/web/scripts/main.js
@@ -91,10 +91,10 @@
                     return E
                 },
                 uploadCallscopeLogs: function() {
-                    return L
+                    return b
                 },
                 uploadCallscopeLogFiles: function() {
-                    return b
+                    return L
                 }
             }), n("222007");
             var o = n("872717"),
@@ -131,7 +131,7 @@
                     blindUserId: r
                 }
             }
-            async function L(t, e, n, o) {
+            async function b(t, e, n, o) {
                 var a;
                 if (!__OVERLAY__) {
                     if ((null === l.default || void 0 === l.default ? void 0 : null === (a = l.default.fileManager) || void 0 === a ? void 0 : a.getCallscopeLogFiles) == null) {
@@ -149,13 +149,13 @@
                         } = await E(t, e, n), i = "channel blind(".concat(t, "): ").concat(a, ", user blind(").concat(e, "): ").concat(r);
                         f.info("uploadCallscopeLogs: Uploading callscope logs for context: ".concat(o, ", ").concat(i));
                         let c = await l.default.fileManager.getCallscopeLogFiles(a);
-                        await b(r, c)
+                        await L(r, c)
                     } catch (t) {
                         f.error("uploadCallscopeLogs: Error uploading logs ".concat(null == t ? void 0 : t.text), t)
                     }
                 }
             }
-            async function b(t, e) {
+            async function L(t, e) {
                 try {
                     if (0 === e.length) {
                         f.error("uploadCallscopeLogFiles: No files found.");
@@ -247,8 +247,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "244686",
-                    versionHash: "ad913f98b104214d082f17ae88ba378f167f13ec"
+                    buildNumber: "244692",
+                    versionHash: "c18b7c86b363bc145469fdde34c5f393b77ec746"
                 }
             }
             n.r(e), n.d(e, {
@@ -357,4 +357,4 @@
         }
     }
 ]);
-//# sourceMappingURL=bbd51a2a379aaabd8d84.js.map
+//# sourceMappingURL=ab5fc7e6205d4458dd13.js.map
--- data/client/channels/ptb/web/scripts/scripts/0a1c85aef610d6b88a03.js
+++ data/client/channels/ptb/web/scripts/scripts/0a1c85aef610d6b88a03.js
@@ -1105,7 +1105,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "ad913f98b104214d082f17ae88ba378f167f13ec"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "c18b7c86b363bc145469fdde34c5f393b77ec746"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
@@ -1175,4 +1175,4 @@
         }
     }
 ]);
-//# sourceMappingURL=e0eefc13133ebbf7d61e.js.map
+//# sourceMappingURL=0a1c85aef610d6b88a03.js.map
