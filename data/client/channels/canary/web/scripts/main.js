edbd25314dd61da58d74ab3989e38bb842188356
xHyroM <github@xhyrom.dev>
2023-11-14 19:30:46 +0000

    📥 Canary Web Build 245526 (a77b8a68f97ed0c443f736adf91862cb6020f0d9)
    Build Number: 245526
    Version Hash: a77b8a68f97ed0c443f736adf91862cb6020f0d9
    Build At: 14/10/2023 19:25:41 (1699989941666)
    
    Scripts (96):
      c13ef44fec53bc7ec2b3.js
      38ec1d57f50f4b16fbc9.js
      f0c7b8c2551bef7bedbe.js
      e92bcd49a35176c74689.js
      275913a14b80649f7304.js
      8add1db2a97e7a1b803a.js
      860941194c36a08ad18e.js
      e8cd1912d9ab669b5640.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      d179fa5fdde237bd188f.js
      8008dff0e97e29f17606.js
      7fc5dac90fa6ced3cf35.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      a2b67f367afb8d818ded.js
      b3d9afb1de73361444d9.js
      549929154a253e35f47f.js
      0832b14af670264d0d6a.js
      77c58eb0c94231b5b0c8.js
      108bf353f6a3d2f5cb03.js
      e351b88b824dbec88711.js
      9499aa6abb8f7b3666b2.js
      345ae7315741c5558539.js
      8d266d9fc7abe4d5a7e9.js
      97598f1cbc36bdd0e3e6.js
      1f70c6eb3686a39a7854.js
      076ed90c0bb9c8c8d90a.js
      76ce3c1701b64d925597.js
      832064086a84f66dd133.js
      2d2fb338c4ce133c5cac.js
      ba60ecac374ac0077c25.js
      9a22220d68f36fd3a498.js
      25dc10f313478679d26b.js
      21313f31f07ab7306ca3.js
      3ba791d5b2a97ee0a4ca.js
      b2171d350a6fc181e8ed.js
      370695f3dfaf71afc0e1.js
      f71cc9f79f08bd3c3257.js
      9221a252a9d51e007cca.js
      791946a348ccd12f1fe5.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      16fa93b0628537fca5c5.js
      bec9d4b88f67814ada9b.js
      e82d8d0fc947a278e60f.js
      d64f7b12dc575d1aa1d1.js
      3dd2689adab042b5a352.js
      d05681f0492239d853d6.js
      7297849048ddd74b7f4f.js
      22e5d3fb0e1c07f17064.js
      d3d33196ec4fb72a32e7.js
      7f8636ea24bd70dff157.js
      7f43433916d6d96572c4.js
      48bcc4d1116e1a70583b.js
      172de3b155f306b773be.js
      1019005833365e6b590e.js
      9cc8fae2a2fd0803b1a2.js
      7c519183a524bc6e1403.js
      206e13d6a40e4104e58d.js
      0d47d515b3149dc2b354.js
      6acda0f7011a6121b457.js
      9e77438d95cf54da592b.js
      e3b24155f058a2dd292f.js
      9d2d7e84d244199e7d27.js
      74e28e8dc215bf8e128b.js
      478ecc39ad990d3c0169.js
      54793f691f7805375f56.js
      587e3f4a405d796eda69.js
      11e0ae849b521f288679.js
      d68a998dc37a19fa3b35.js
      56669914bde6563850fa.js
      3229901073f7f5246652.js
      e5d3321c499c33bd0c5b.js
    * e2c5c34d3c9dedb78ae0.js
      05742b63f3a02e10537f.js
      8a36e06ec1d0bf91a04a.js
      a1e0fbf8f9cbab9b443c.js
      bbf702ad6ca7df9145d8.js
      84112e944bd8e0af2d10.js
      736aab6e9f0251482a2a.js
      bc682dbd7e06417e3e0c.js
      3cacdf0beb3513eb5d51.js
      a5da41c4e621479511cb.js
      cfc178311342ccf054bc.js
      e1e5cc7fe127e6611cad.js
      2f6684d6f3b07fd42cd8.js
      be07a459b7152808eb4b.js
      e44e5b8e5f096f542a5b.js
    
    Stylesheets (2):
      shared.16d04fc9a65da07817b6.css
    * app.d1e1b2971c0bdb8bc99b.css

--- data/client/channels/canary/web/scripts/main.js
+++ data/client/channels/canary/web/scripts/main.js
@@ -91,10 +91,10 @@
                     return E
                 },
                 uploadCallscopeLogs: function() {
-                    return b
+                    return L
                 },
                 uploadCallscopeLogFiles: function() {
-                    return L
+                    return b
                 }
             }), n("222007");
             var o = n("872717"),
@@ -131,7 +131,7 @@
                     blindUserId: r
                 }
             }
-            async function b(t, e, n, o) {
+            async function L(t, e, n, o) {
                 var a;
                 if (!__OVERLAY__) {
                     if ((null === l.default || void 0 === l.default ? void 0 : null === (a = l.default.fileManager) || void 0 === a ? void 0 : a.getCallscopeLogFiles) == null) {
@@ -149,13 +149,13 @@
                         } = await E(t, e, n), i = "channel blind(".concat(t, "): ").concat(a, ", user blind(").concat(e, "): ").concat(r);
                         f.info("uploadCallscopeLogs: Uploading callscope logs for context: ".concat(o, ", ").concat(i));
                         let c = await l.default.fileManager.getCallscopeLogFiles(a);
-                        await L(r, c)
+                        await b(r, c)
                     } catch (t) {
                         f.error("uploadCallscopeLogs: Error uploading logs ".concat(null == t ? void 0 : t.text), t)
                     }
                 }
             }
-            async function L(t, e) {
+            async function b(t, e) {
                 try {
                     if (0 === e.length) {
                         f.error("uploadCallscopeLogFiles: No files found.");
@@ -247,8 +247,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "245518",
-                    versionHash: "a0eb89c5ba0754584fcffff3f766823126ba1db7"
+                    buildNumber: "245526",
+                    versionHash: "a77b8a68f97ed0c443f736adf91862cb6020f0d9"
                 }
             }
             n.r(e), n.d(e, {
@@ -357,4 +357,4 @@
         }
     }
 ]);
-//# sourceMappingURL=bc6b93d5b0f3ca0d1da6.js.map
+//# sourceMappingURL=e2c5c34d3c9dedb78ae0.js.map
--- data/client/channels/canary/web/scripts/scripts/8a36e06ec1d0bf91a04a.js
+++ data/client/channels/canary/web/scripts/scripts/8a36e06ec1d0bf91a04a.js
@@ -2376,7 +2376,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-a0eb89c5ba0754584fcffff3f766823126ba1db7",
+                    release: "discord_web-a77b8a68f97ed0c443f736adf91862cb6020f0d9",
                     beforeSend: e => {
                         var t, n;
                         return !(null != (t = e).exception && null != t.exception.values && t.exception.values.every(e => null == e.stacktrace || null != e.stacktrace.frames && 1 === e.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || s.some(e => window.navigator.appVersion.toLowerCase().indexOf(e) >= 0)) && !i() && !("Aborted" === (n = e).message || "cancel captcha" === n.message) && l() ? e : null
@@ -2394,7 +2394,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), a.setTag("buildNumber", (e = "245518", "245518")), a.setTag("builtAt", String("1699989068775"));
+                }), a.setTag("buildNumber", (e = "245526", "245526")), a.setTag("builtAt", String("1699989941666"));
                 let t = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != t && "object" == typeof t)
                     for (let e in t) a.setTag(e, t[e]);
@@ -4870,4 +4870,4 @@
         }
     }
 ]);
-//# sourceMappingURL=8380233ce83e777eb98e.js.map
+//# sourceMappingURL=8a36e06ec1d0bf91a04a.js.map
