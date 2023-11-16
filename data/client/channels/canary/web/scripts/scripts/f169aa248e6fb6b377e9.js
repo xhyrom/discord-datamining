d8d24b00afffe1c2e98c6d40174917864bad7127
xHyroM <github@xhyrom.dev>
2023-11-16 03:24:15 +0000

    📥 Canary Web Build 246202 (a610f3cc86d309d00f22efbbe356ac9ea34e1f14)
    Build Number: 246202
    Version Hash: a610f3cc86d309d00f22efbbe356ac9ea34e1f14
    Build At: 16/10/2023 3:12:8 (1700104328381)
    
    Scripts (98):
      c13ef44fec53bc7ec2b3.js
      f35b7ca5174bcdbfe242.js
      f0c7b8c2551bef7bedbe.js
      e92bcd49a35176c74689.js
      275913a14b80649f7304.js
      8add1db2a97e7a1b803a.js
      860941194c36a08ad18e.js
      e8cd1912d9ab669b5640.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      d179fa5fdde237bd188f.js
      8008dff0e97e29f17606.js
      7fc5dac90fa6ced3cf35.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      717531d4d2aa46ed5d3f.js
      b3d9afb1de73361444d9.js
      549929154a253e35f47f.js
      0832b14af670264d0d6a.js
      77c58eb0c94231b5b0c8.js
      108bf353f6a3d2f5cb03.js
      e351b88b824dbec88711.js
      588c4c07affe6a3e653d.js
      345ae7315741c5558539.js
      8d266d9fc7abe4d5a7e9.js
      97598f1cbc36bdd0e3e6.js
      1f70c6eb3686a39a7854.js
      76ce3c1701b64d925597.js
      e457124ec109d9782734.js
      2d2fb338c4ce133c5cac.js
      18364b8996bfd4b9203f.js
      9a22220d68f36fd3a498.js
      25dc10f313478679d26b.js
      96ecb347fa52f52688e3.js
      3ba791d5b2a97ee0a4ca.js
      b2171d350a6fc181e8ed.js
      f5d379cb13ab4bbfb1ed.js
      370695f3dfaf71afc0e1.js
      ba60ecac374ac0077c25.js
      d564031e4aff40b7e5f5.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      bec9d4b88f67814ada9b.js
      631462826643175a32e4.js
      e82d8d0fc947a278e60f.js
      b00b02202e71c00e412d.js
      16fa93b0628537fca5c5.js
      33e6144825cc615c27a1.js
      058fecdc6256ae1e97d9.js
      1cfd804a60fb4b49cd37.js
      7297849048ddd74b7f4f.js
      22e5d3fb0e1c07f17064.js
      7f43433916d6d96572c4.js
      d3d33196ec4fb72a32e7.js
      7f8636ea24bd70dff157.js
      85fe1ecf0ab8c295babe.js
      172de3b155f306b773be.js
      48bcc4d1116e1a70583b.js
      1019005833365e6b590e.js
      0d47d515b3149dc2b354.js
      791091759747aaf574e8.js
      206e13d6a40e4104e58d.js
      9e77438d95cf54da592b.js
      6acda0f7011a6121b457.js
      ef7fab730fc37d0f37b4.js
      38f743f676188eac48b5.js
      74e28e8dc215bf8e128b.js
      1d2e76ed6c567cf6872d.js
      587e3f4a405d796eda69.js
      8dac239e96f8217c8b7f.js
      11e0ae849b521f288679.js
      1d499b6f39a82af93f11.js
      3229901073f7f5246652.js
      f169aa248e6fb6b377e9.js
      e5d3321c499c33bd0c5b.js
      736aab6e9f0251482a2a.js
      05742b63f3a02e10537f.js
    * 73cf31ca0eca6c657e62.js
      21136b17970144534f1f.js
      5a72cecfbd7ca3c5416c.js
      652810fe62b532600c6f.js
      bbf702ad6ca7df9145d8.js
      84112e944bd8e0af2d10.js
      bc682dbd7e06417e3e0c.js
      d480dd70c11216107f76.js
      2a5d6d25b3f9b1c3fc9a.js
      02caa704ca310b912474.js
      2f6684d6f3b07fd42cd8.js
      c77dcff32547a140a014.js
      abf1f5f4d5dc50c08efa.js
    
    Stylesheets (2):
      shared.52d9520089ae820788ba.css
    * app.c909f063ebabe2a0b861.css

--- data/client/channels/canary/web/scripts/scripts/f169aa248e6fb6b377e9.js
+++ data/client/channels/canary/web/scripts/scripts/f169aa248e6fb6b377e9.js
@@ -5287,7 +5287,7 @@
                 let i = {},
                     r = window.GLOBAL_ENV.RELEASE_CHANNEL;
                 r && (i.release_channel = r.split("-")[0]);
-                let o = parseInt((n = "246191", "246191"), 10);
+                let o = parseInt((n = "246202", "246202"), 10);
                 !isNaN(o) && (i.client_build_number = o);
                 let s = null == N ? void 0 : null === (e = (t = N.remoteApp).getBuildNumber) || void 0 === e ? void 0 : e.call(t);
                 return !isNaN(s) && (i.native_build_number = s), i.client_event_source = function() {
@@ -5529,4 +5529,4 @@
         }
     }
 ]);
-//# sourceMappingURL=9af70d0f7a84222c94af.js.map
+//# sourceMappingURL=f169aa248e6fb6b377e9.js.map
