22debd9a01f7e5408553c1f6f8c7dc6e81479a8c
xHyroM <github@xhyrom.dev>
2023-11-21 03:11:07 +0000

    📥 Public Testing Beta Web Build 247684 (be951024711a00f6a1d2235c001b143e79c9b3b9)
    Build Number: 247684
    Version Hash: be951024711a00f6a1d2235c001b143e79c9b3b9
    Build At: 21/10/2023 3:4:19 (1700535859941)
    
    Scripts (99):
      c13ef44fec53bc7ec2b3.js
      bb8c97fb9834488b8e0b.js
      f0c7b8c2551bef7bedbe.js
      e92bcd49a35176c74689.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      275913a14b80649f7304.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      717531d4d2aa46ed5d3f.js
      b3d9afb1de73361444d9.js
      44db884d67d972688810.js
      cd849b5ff4b99403065f.js
      7d007ca50c3976f4a860.js
      247c7c9950ec67e3fca2.js
      375056d03774410e82bd.js
      218aeb3cc5a47fa3deb7.js
      1a4ada1eb5043bbc43a2.js
      c62ad3a359aec3fe81e2.js
      1e03ea72739fa2ea607c.js
      1f70c6eb3686a39a7854.js
      a6eb6190a5ddf1e47ccd.js
      9a22220d68f36fd3a498.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      370695f3dfaf71afc0e1.js
      2d1ee5c3d0c67befe6c9.js
      18364b8996bfd4b9203f.js
      25dc10f313478679d26b.js
      1eea288afea794d79086.js
      c25018b5062b1a633ab9.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      eec0a014021709806e99.js
      25f4465a5663faa10637.js
      22dcb4488c171f5f21d0.js
      7dbcf0ab8255b8e5ac0a.js
      0754801f9ce389728091.js
      631462826643175a32e4.js
      c092f876b32bafd56a0a.js
      30a0ff619dec9f0c30ff.js
      ba3b5bf51029226c1b1b.js
      11c94edae9045639c544.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      172de3b155f306b773be.js
      d3d33196ec4fb72a32e7.js
      7f43433916d6d96572c4.js
      703e6ebf759ec4911d43.js
      88b305b582981b7c2042.js
      fd027f17dca07f20ba5a.js
      5b67aca2ff6d61a81bac.js
      64155ff4f27459bc3229.js
      3fa96f227706fe0a6ea8.js
      9139efb05921c2499969.js
      48ded73a11058e68607c.js
      be87869bc58dac64b815.js
      9ffc23460888d3048a77.js
      0bd08896e7368e7251c1.js
      74e28e8dc215bf8e128b.js
      f76be1d4b4998a8405f1.js
      587e3f4a405d796eda69.js
      793dc7391c42462376fc.js
      8dac239e96f8217c8b7f.js
      11e0ae849b521f288679.js
      ecd6f51d49fa1651e97b.js
      3229901073f7f5246652.js
      16439c6eec182b8ad3e2.js
      d4a9b50354120eb527b3.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * fedcebd75cb749e8ddc2.js
      d4d6a97ab2724106358f.js
      7e36448b9d21233f918b.js
      652810fe62b532600c6f.js
      ccc46ac89f58bfac7efe.js
      47a38e481a7712a70ef9.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      b124a04354d116bdbe60.js
      0909d59426dd6ffd8a56.js
      2f6684d6f3b07fd42cd8.js
      7918b949d286dc62c54f.js
      5c6f4bbd65a29defb785.js
    
    Stylesheets (2):
      shared.d75109920b8828e7909e.css
    * app.60345ce3b5a846e63ccb.css

--- data/client/channels/ptb/web/scripts/scripts/0909d59426dd6ffd8a56.js
+++ data/client/channels/ptb/web/scripts/scripts/0909d59426dd6ffd8a56.js
@@ -6096,7 +6096,7 @@
                         opacity: 1
                     },
                     [a.LIGHT]: {
-                        raw: "YELLOW_430",
+                        raw: "YELLOW_300",
                         opacity: 1
                     },
                     [a.AMOLED]: {
@@ -6150,7 +6150,7 @@
                         opacity: 1
                     },
                     [a.LIGHT]: {
-                        raw: "GREEN_430",
+                        raw: "GREEN_330",
                         opacity: 1
                     },
                     [a.AMOLED]: {
@@ -13003,4 +13003,4 @@
         }
     }
 ]);
-//# sourceMappingURL=6193a103e5546653a8ee.js.map
+//# sourceMappingURL=0909d59426dd6ffd8a56.js.map
