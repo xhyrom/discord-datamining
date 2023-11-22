29dd1629c4ba44bbcee1be7dc1e34d5cfbbb4b34
xHyroM <github@xhyrom.dev>
2023-11-22 23:42:20 +0000

    📥 Public Testing Beta Web Build 248434 (5228164c0b328984ded2379773c4e07a6fee539e)
    Build Number: 248434
    Version Hash: 5228164c0b328984ded2379773c4e07a6fee539e
    Build At: 22/10/2023 23:37:25 (1700696245938)
    
    Scripts (99):
      c754f4cf4826e1eb80c4.js
      128f54b47dbfc4d8f1e5.js
      6e0f2743425529ec9a3e.js
      e92bcd49a35176c74689.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      275913a14b80649f7304.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      7c1dc3a41650229f2417.js
      b3d9afb1de73361444d9.js
      4b8086d5c3d130231bbc.js
      cd849b5ff4b99403065f.js
      7d007ca50c3976f4a860.js
      0acee052fc9def9f05d1.js
      2a95edb859e1d456cbe9.js
      218aeb3cc5a47fa3deb7.js
      1a4ada1eb5043bbc43a2.js
      cdd7376308f6262346cf.js
      1e03ea72739fa2ea607c.js
      1f70c6eb3686a39a7854.js
      bf9d4ac83f8bf92be1ea.js
      9a22220d68f36fd3a498.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      370695f3dfaf71afc0e1.js
      2d1ee5c3d0c67befe6c9.js
      18364b8996bfd4b9203f.js
      25dc10f313478679d26b.js
      95dc2e9e820820967555.js
      1eea288afea794d79086.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      2941169f9c1baffc3e8a.js
      25f4465a5663faa10637.js
      22dcb4488c171f5f21d0.js
      354de5ec5a0ac73e08ab.js
      0754801f9ce389728091.js
      631462826643175a32e4.js
      e618571a1c8b790bd9e5.js
      6532bca0c1d972aa5e84.js
      6171ccd6f64857b72358.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      172de3b155f306b773be.js
      7105d83fb6dfbed18bba.js
      7f43433916d6d96572c4.js
      00fb0f84aa5ede0c7e17.js
      64530193308909fcec0a.js
      92aea76170ca20ee760d.js
      985d37b6f415ba428cbd.js
      67311e38d3355e7762dd.js
      64155ff4f27459bc3229.js
      fe470f402843fa79965c.js
      9139efb05921c2499969.js
      48ded73a11058e68607c.js
      9ffc23460888d3048a77.js
      be87869bc58dac64b815.js
      ff54cbfd2776d0ecc74d.js
      74e28e8dc215bf8e128b.js
      f76be1d4b4998a8405f1.js
      587e3f4a405d796eda69.js
      747fcc1d5b2ad6be69e2.js
      9e9846439c03b27aec8a.js
      11e0ae849b521f288679.js
      5368be1274d2d9ce9cd3.js
      3229901073f7f5246652.js
      6407fa435672468879f0.js
      d4a9b50354120eb527b3.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * 00f0faeac74b420b3aa9.js
      2d21226ffbc90cdf42cb.js
      167c9c0feee477bf9b1b.js
      652810fe62b532600c6f.js
      ccc46ac89f58bfac7efe.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      47a38e481a7712a70ef9.js
      2e350e4a01b1f62e58ce.js
      00df6db02b4a5b3f79fd.js
      2f6684d6f3b07fd42cd8.js
      7dfee69ed8a142cfe3e4.js
      f8347d21f3507724ad7e.js
    
    Stylesheets (2):
      shared.c12a48e4fe7372a51217.css
    * app.f4e71538c961fca0795e.css

--- data/client/channels/ptb/web/scripts/scripts/747fcc1d5b2ad6be69e2.js
+++ data/client/channels/ptb/web/scripts/scripts/747fcc1d5b2ad6be69e2.js
@@ -14492,7 +14492,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "248429"
+                                build_number: "248434"
                             },
                             h = l.default.getCurrentUser();
                         null != h && (c.user_id = h.id, c.user_name = h.tag, null != h.email && (c.email = h.email));
@@ -21306,4 +21306,4 @@
         }
     }
 ]);
-//# sourceMappingURL=736e5f08afe380174b7f.js.map
+//# sourceMappingURL=747fcc1d5b2ad6be69e2.js.map
--- data/client/channels/ptb/web/scripts/scripts/fe470f402843fa79965c.js
+++ data/client/channels/ptb/web/scripts/scripts/fe470f402843fa79965c.js
@@ -1410,7 +1410,7 @@
                 l = n.n(r),
                 a = n("862337"),
                 s = n("296892");
-            let u = new Date("2024-01-01T07:59:59.000Z"),
+            let u = new Date("2024-01-04T07:59:59.000Z"),
                 o = l(u),
                 d = (0, s.default)({
                     id: "2023-11_seasonal_gifting",
@@ -1947,4 +1947,4 @@
         }
     }
 ]);
-//# sourceMappingURL=67c81b80fba8eacf0285.js.map
+//# sourceMappingURL=fe470f402843fa79965c.js.map
