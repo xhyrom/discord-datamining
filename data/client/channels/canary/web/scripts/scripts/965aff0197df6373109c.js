a20b88de671372968b0ce9cf15e21cc27565ecde
xHyroM <github@xhyrom.dev>
2023-11-22 20:14:47 +0000

    📥 Canary Web Build 248310 (67fb97e7045fd56c5ea4ff1f8a084d38dd0bc359)
    Build Number: 248310
    Version Hash: 67fb97e7045fd56c5ea4ff1f8a084d38dd0bc359
    Build At: 22/10/2023 20:6:28 (1700683588430)
    
    Scripts (99):
      c754f4cf4826e1eb80c4.js
      b8addcc7de15509ea1fc.js
      6e0f2743425529ec9a3e.js
      e92bcd49a35176c74689.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      275913a14b80649f7304.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      7c1dc3a41650229f2417.js
      b3d9afb1de73361444d9.js
      4b8086d5c3d130231bbc.js
      cd849b5ff4b99403065f.js
      7d007ca50c3976f4a860.js
      0acee052fc9def9f05d1.js
      a21d02f3f92db9eeb0a3.js
      218aeb3cc5a47fa3deb7.js
      1a4ada1eb5043bbc43a2.js
      c62ad3a359aec3fe81e2.js
      1e03ea72739fa2ea607c.js
      1f70c6eb3686a39a7854.js
      593731518a57129b84d4.js
      9a22220d68f36fd3a498.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      370695f3dfaf71afc0e1.js
      2d1ee5c3d0c67befe6c9.js
      18364b8996bfd4b9203f.js
      25dc10f313478679d26b.js
      95dc2e9e820820967555.js
      1eea288afea794d79086.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      2941169f9c1baffc3e8a.js
      25f4465a5663faa10637.js
      22dcb4488c171f5f21d0.js
      354de5ec5a0ac73e08ab.js
      0754801f9ce389728091.js
      631462826643175a32e4.js
      e618571a1c8b790bd9e5.js
      6532bca0c1d972aa5e84.js
      6171ccd6f64857b72358.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      172de3b155f306b773be.js
      7105d83fb6dfbed18bba.js
      7f43433916d6d96572c4.js
      2d6c5bdb5c5a02056673.js
      e90e87c357e877046cf3.js
      67311e38d3355e7762dd.js
      27893ea7e862b3b6186d.js
      985d37b6f415ba428cbd.js
      64155ff4f27459bc3229.js
      67c81b80fba8eacf0285.js
      9139efb05921c2499969.js
      be87869bc58dac64b815.js
      48ded73a11058e68607c.js
      9ffc23460888d3048a77.js
      db1a4ebdcdbc1a07afdb.js
      74e28e8dc215bf8e128b.js
      f76be1d4b4998a8405f1.js
      587e3f4a405d796eda69.js
      965aff0197df6373109c.js
      9e9846439c03b27aec8a.js
      11e0ae849b521f288679.js
      5368be1274d2d9ce9cd3.js
      3229901073f7f5246652.js
      e7947bd69b8335388c53.js
      d4a9b50354120eb527b3.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * f516e6fecc2ace38dd6a.js
      6bc11bab451c51f6c39e.js
      167c9c0feee477bf9b1b.js
      652810fe62b532600c6f.js
      ccc46ac89f58bfac7efe.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      47a38e481a7712a70ef9.js
      2e350e4a01b1f62e58ce.js
      588e9f5ed26e98933316.js
      2f6684d6f3b07fd42cd8.js
      d7b1f842e508f10e562f.js
      a0d20cf9fe8b734cb7ba.js
    
    Stylesheets (2):
      shared.c12a48e4fe7372a51217.css
    * app.5d5cbb313d3963ac9375.css

--- data/client/channels/canary/web/scripts/scripts/965aff0197df6373109c.js
+++ data/client/channels/canary/web/scripts/scripts/965aff0197df6373109c.js
@@ -14492,7 +14492,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "248278"
+                                build_number: "248310"
                             },
                             h = l.default.getCurrentUser();
                         null != h && (c.user_id = h.id, c.user_name = h.tag, null != h.email && (c.email = h.email));
@@ -21306,4 +21306,4 @@
         }
     }
 ]);
-//# sourceMappingURL=fcdbc9966ed61308f503.js.map
+//# sourceMappingURL=965aff0197df6373109c.js.map
