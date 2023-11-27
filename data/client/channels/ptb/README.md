59467773bc201113d22146f1ee9860754843d25e
xHyroM <github@xhyrom.dev>
2023-11-27 21:46:07 +0000

    📥 Public Testing Beta Web Build 248768 (f346588bfe58b3511d94d61bb10b8eb8ada01d1b)
    Build Number: 248768
    Version Hash: f346588bfe58b3511d94d61bb10b8eb8ada01d1b
    Build At: 27/10/2023 21:38:4 (1701121084085)
    
    Scripts (99):
      c754f4cf4826e1eb80c4.js
      75afd91506071991196d.js
      6e0f2743425529ec9a3e.js
      e92bcd49a35176c74689.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      275913a14b80649f7304.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      7c1dc3a41650229f2417.js
      b3d9afb1de73361444d9.js
      4b8086d5c3d130231bbc.js
      cd849b5ff4b99403065f.js
      7d007ca50c3976f4a860.js
      0acee052fc9def9f05d1.js
      2a95edb859e1d456cbe9.js
      218aeb3cc5a47fa3deb7.js
      1a4ada1eb5043bbc43a2.js
      cdd7376308f6262346cf.js
      1e03ea72739fa2ea607c.js
      1f70c6eb3686a39a7854.js
      1c37aa2abedff4420734.js
      9a22220d68f36fd3a498.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      18364b8996bfd4b9203f.js
      370695f3dfaf71afc0e1.js
      2d1ee5c3d0c67befe6c9.js
      25dc10f313478679d26b.js
      1eea288afea794d79086.js
      95dc2e9e820820967555.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      2941169f9c1baffc3e8a.js
      25f4465a5663faa10637.js
      22dcb4488c171f5f21d0.js
      354de5ec5a0ac73e08ab.js
      0754801f9ce389728091.js
      631462826643175a32e4.js
      90e01c38477691548a61.js
      6532bca0c1d972aa5e84.js
      6171ccd6f64857b72358.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      172de3b155f306b773be.js
      7105d83fb6dfbed18bba.js
      7f43433916d6d96572c4.js
      00fb0f84aa5ede0c7e17.js
      44861421c4d872e50018.js
      67311e38d3355e7762dd.js
      00173ffe6a17f813c779.js
      985d37b6f415ba428cbd.js
      64155ff4f27459bc3229.js
      fe470f402843fa79965c.js
      9139efb05921c2499969.js
      be87869bc58dac64b815.js
      48ded73a11058e68607c.js
      9ffc23460888d3048a77.js
      95a12a4d4f2ea8a373a5.js
      74e28e8dc215bf8e128b.js
      f76be1d4b4998a8405f1.js
      587e3f4a405d796eda69.js
      e6d9137bb310d057a910.js
      9e9846439c03b27aec8a.js
      11e0ae849b521f288679.js
      5368be1274d2d9ce9cd3.js
      3229901073f7f5246652.js
      ca9a79efe39be96336d3.js
      d4a9b50354120eb527b3.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * 903b7dfb78423778187a.js
      f831b0ecb0ae607cacc1.js
      167c9c0feee477bf9b1b.js
      652810fe62b532600c6f.js
      ccc46ac89f58bfac7efe.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      47a38e481a7712a70ef9.js
      2e350e4a01b1f62e58ce.js
      be7a14e71e35c02c7d82.js
      874469d33a07300b4ed8.js
      013b1826e64129892883.js
      42c331f7dfcfba3b1a3a.js
    
    Stylesheets (2):
      shared.b873b88ac11414513738.css
    * app.068e540c73de5c3c2adb.css

--- data/client/channels/ptb/README.md
+++ data/client/channels/ptb/README.md
@@ -1,10 +1,10 @@
 # Public Testing Beta
 
 ### Info
-Build number: 248763  
-Version hash: f157dc0898cc984b55751e818d8e6d5b4ec16096  
+Build number: 248768  
+Version hash: f346588bfe58b3511d94d61bb10b8eb8ada01d1b  
 Host version: 1.0.1045  
-Built at: 11/27/2023, 9:35:16 PM  
+Built at: 11/27/2023, 9:38:04 PM  
 
 ### Modules
 | Module               | Version | Package sha256                                                   | URL                                                                                              |
--- data/client/channels/ptb/web/scripts/scripts/e6d9137bb310d057a910.js
+++ data/client/channels/ptb/web/scripts/scripts/e6d9137bb310d057a910.js
@@ -14548,7 +14548,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "248763"
+                                build_number: "248768"
                             },
                             h = l.default.getCurrentUser();
                         null != h && (c.user_id = h.id, c.user_name = h.tag, null != h.email && (c.email = h.email));
@@ -21362,4 +21362,4 @@
         }
     }
 ]);
-//# sourceMappingURL=ed95d841469811f78070.js.map
+//# sourceMappingURL=e6d9137bb310d057a910.js.map
