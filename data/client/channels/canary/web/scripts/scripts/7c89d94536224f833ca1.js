39ccde79ac14fa7b1d4ed27f3c99f46b95ce88b0
xHyroM <github@xhyrom.dev>
2023-11-28 16:34:12 +0000

    📥 Canary Web Build 249125 (b2b942b7ce34123cfd7b0c12532f62ea5dbe4358)
    Build Number: 249125
    Version Hash: b2b942b7ce34123cfd7b0c12532f62ea5dbe4358
    Build At: 28/10/2023 16:16:28 (1701188188213)
    
    Scripts (99):
      c754f4cf4826e1eb80c4.js
      30811057142673e7a290.js
      6e0f2743425529ec9a3e.js
      e92bcd49a35176c74689.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      275913a14b80649f7304.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      7c1dc3a41650229f2417.js
      b3d9afb1de73361444d9.js
      4b8086d5c3d130231bbc.js
      be6130b9062a2cc46dc6.js
      7d007ca50c3976f4a860.js
      0acee052fc9def9f05d1.js
      2a95edb859e1d456cbe9.js
      218aeb3cc5a47fa3deb7.js
      1a4ada1eb5043bbc43a2.js
      cdd7376308f6262346cf.js
      1e03ea72739fa2ea607c.js
      1f70c6eb3686a39a7854.js
      4ce23e9bd9b8e316c5f3.js
      9a22220d68f36fd3a498.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      18364b8996bfd4b9203f.js
      370695f3dfaf71afc0e1.js
      2d1ee5c3d0c67befe6c9.js
      25dc10f313478679d26b.js
      1eea288afea794d79086.js
      95dc2e9e820820967555.js
      f973e023961930ea16d6.js
      63ffba8e93a7f7585df3.js
      2941169f9c1baffc3e8a.js
      25f4465a5663faa10637.js
      22dcb4488c171f5f21d0.js
      0754801f9ce389728091.js
      631462826643175a32e4.js
      70c61bbf938b67021eca.js
      414c73728c61d48ca94d.js
      6532bca0c1d972aa5e84.js
      6171ccd6f64857b72358.js
      22e5d3fb0e1c07f17064.js
      7297849048ddd74b7f4f.js
      172de3b155f306b773be.js
      7f43433916d6d96572c4.js
      00fb0f84aa5ede0c7e17.js
      df41c1fde60f583d2e9f.js
      7105d83fb6dfbed18bba.js
      9fa9421a38998e2ec24c.js
      985d37b6f415ba428cbd.js
      00173ffe6a17f813c779.js
      64155ff4f27459bc3229.js
      fe470f402843fa79965c.js
      9139efb05921c2499969.js
      48ded73a11058e68607c.js
      9ffc23460888d3048a77.js
      be87869bc58dac64b815.js
      b35d4a309cdc2de7efe4.js
      74e28e8dc215bf8e128b.js
      f76be1d4b4998a8405f1.js
      587e3f4a405d796eda69.js
      7c89d94536224f833ca1.js
      9e9846439c03b27aec8a.js
      11e0ae849b521f288679.js
      69d84d427ca2ee2c4521.js
      3229901073f7f5246652.js
      b71367164809c1c8fe27.js
      d4a9b50354120eb527b3.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * 4cf69ada3c9bd8717e4a.js
      d1f820905b010a636803.js
      167c9c0feee477bf9b1b.js
      652810fe62b532600c6f.js
      ccc46ac89f58bfac7efe.js
      47a38e481a7712a70ef9.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      039d02f4dd80c4e60971.js
      8cc616dea204e7821815.js
      874469d33a07300b4ed8.js
      7204ed5d844edc5affa3.js
      4c7c7cb9f4ff41a1c9be.js
    
    Stylesheets (2):
      shared.b873b88ac11414513738.css
    * app.5296e6a4d7a5e7ce7103.css

--- data/client/channels/canary/web/scripts/scripts/7c89d94536224f833ca1.js
+++ data/client/channels/canary/web/scripts/scripts/7c89d94536224f833ca1.js
@@ -1,5 +1,5 @@
 (this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
-    ["89163"], {
+    ["48449"], {
         662697: function(e, t, n) {
             let i = {};
             i.isSafari = function() {
@@ -14607,7 +14607,7 @@
                         var i;
                         let c = {
                                 environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                                build_number: "249116"
+                                build_number: "249125"
                             },
                             h = l.default.getCurrentUser();
                         null != h && (c.user_id = h.id, c.user_name = h.tag, null != h.email && (c.email = h.email));
@@ -21421,4 +21421,4 @@
         }
     }
 ]);
-//# sourceMappingURL=67a91fbd860196e9513f.js.map
+//# sourceMappingURL=7c89d94536224f833ca1.js.map
