f315e9e2d264993d0cfac349865c82229d500229
xHyroM <github@xhyrom.dev>
2023-11-30 22:41:17 +0000

    📥 Public Testing Beta Web Build 250298 (1b41c555d7750b4d275970a5baf08c9348bf4322)
    Build Number: 250298
    Version Hash: 1b41c555d7750b4d275970a5baf08c9348bf4322
    Build At: 30/10/2023 22:34:43 (1701383683944)
    
    Scripts (98):
      0a5377e50363fae69378.js
      e8cd715b155db65dbe41.js
      6e0f2743425529ec9a3e.js
      e92bcd49a35176c74689.js
      275913a14b80649f7304.js
      e8cd1912d9ab669b5640.js
      8add1db2a97e7a1b803a.js
      860941194c36a08ad18e.js
      a76236d61f3388b9edb0.js
      d13babb482ba16a6291d.js
      1c7604029adcff18465c.js
      ee177b1b42fe5f17b75c.js
      cd52d4230f2ee3c11e98.js
      8008dff0e97e29f17606.js
      d179fa5fdde237bd188f.js
      f9b6bf3e8b29f0f8437f.js
      f761fa611daf84d1dd89.js
      40fad28f01da56375e6a.js
      10e39c882223d2a5f274.js
      94f1ed816761b6c86276.js
      bfd4701fcb8772780564.js
      25b5e8687935019950ef.js
      edc45ace92bb9a62c3c3.js
      1970a2549561cf89cefa.js
      7c1dc3a41650229f2417.js
      b3d9afb1de73361444d9.js
      dc63d2e5a0b1e1a03c50.js
      5241a8e32c18d6fbacd9.js
      7d007ca50c3976f4a860.js
      885a01187990460e739d.js
      2a95edb859e1d456cbe9.js
      218aeb3cc5a47fa3deb7.js
      48d21634838d5069ac06.js
      0b01de389e338575db6d.js
      caceccb05ff1808b4296.js
      1f70c6eb3686a39a7854.js
      eaf0d1eff5b79dc3c6d3.js
      db5980710ac12e567cc9.js
      76ce3c1701b64d925597.js
      d9300213127d7f9bc704.js
      3338382dff3c62f24172.js
      2d1ee5c3d0c67befe6c9.js
      75b020ce48e3bfec06e9.js
      25dc10f313478679d26b.js
      c889c0722312213a62b8.js
      a754b912638afe0e837f.js
      8a5ca33e10d42e321410.js
      63ffba8e93a7f7585df3.js
      6006328c5f4130fc6984.js
      25f4465a5663faa10637.js
      1d6a6ae7f430e1005959.js
      652b9e1f94a5c9e1755a.js
      919135beac2ba698218a.js
      631462826643175a32e4.js
      7e8a54042b494fcbf9fc.js
      bf74e4df46cb00bbdcce.js
      ae678e5a8ee46cc7311d.js
      22e5d3fb0e1c07f17064.js
      78cd558b7f08b9a6d3ef.js
      03116aebc54b89009a30.js
      0d3b5ea245cf4bcd0d0b.js
      cb0cdfb38973b18b884b.js
      aaed5add3627f0f4509b.js
      7105d83fb6dfbed18bba.js
      dac25ad7e04e5fb34c3d.js
      835c8a646dfbf4da9e65.js
      985d37b6f415ba428cbd.js
      64155ff4f27459bc3229.js
      7b650df1a5ac439484ca.js
      023c0b85f8bf8ccc5a95.js
      48ded73a11058e68607c.js
      d80e58c6681893b13d0d.js
      9ffc23460888d3048a77.js
      cd68dee2248cda54ad56.js
      3662717f5db3285fb6f4.js
      0bdc288614d03f7695dc.js
      587e3f4a405d796eda69.js
      783b9eafaee7d4ea9d93.js
      d6f7fe7163eeed651f47.js
      11e0ae849b521f288679.js
      1ab1972edeac2174253e.js
      3229901073f7f5246652.js
      d94635c08ff5d57fd2ff.js
      8ce2fd2658552c79ffca.js
      5816c03bb7375e536dca.js
      05742b63f3a02e10537f.js
    * 9179e678417290958c98.js
      21d0323ca61674407dc6.js
      ab2ab52c89ea6508361e.js
      652810fe62b532600c6f.js
      f2e52435130001da88ba.js
      bc682dbd7e06417e3e0c.js
      0d63e593eac025fbf42b.js
      769fa950c0222f7389ce.js
      13fdba74ced434ba07e2.js
      b325ea72fadef633f4b0.js
      353708fd97006ce9e389.js
      02f75d80329a15ee742c.js
    
    Stylesheets (2):
      shared.b7fadacb16ddeaabaa11.css
    * app.081769f9f449015d00ad.css

--- data/client/channels/ptb/web/scripts/scripts/21d0323ca61674407dc6.js
+++ data/client/channels/ptb/web/scripts/scripts/21d0323ca61674407dc6.js
@@ -2418,7 +2418,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-259fa349063c1fd443dd6a6373052e3a91a780bd",
+                    release: "discord_web-1b41c555d7750b4d275970a5baf08c9348bf4322",
                     beforeSend: e => {
                         var t, n;
                         return !(null != (t = e).exception && null != t.exception.values && t.exception.values.every(e => null == e.stacktrace || null != e.stacktrace.frames && 1 === e.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || s.some(e => window.navigator.appVersion.toLowerCase().indexOf(e) >= 0)) && !i() && !("Aborted" === (n = e).message || "cancel captcha" === n.message) && l() ? e : null
@@ -2436,7 +2436,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), a.setTag("buildNumber", (e = "250295", "250295")), a.setTag("builtAt", String("1701383461502"));
+                }), a.setTag("buildNumber", (e = "250298", "250298")), a.setTag("builtAt", String("1701383683944"));
                 let t = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != t && "object" == typeof t)
                     for (let e in t) a.setTag(e, t[e]);
@@ -4961,4 +4961,4 @@
         }
     }
 ]);
-//# sourceMappingURL=ac7c7f22a0fb396ea427.js.map
+//# sourceMappingURL=21d0323ca61674407dc6.js.map
--- data/client/channels/ptb/web/scripts/scripts/cd68dee2248cda54ad56.js
+++ data/client/channels/ptb/web/scripts/scripts/cd68dee2248cda54ad56.js
@@ -1136,7 +1136,7 @@
                     } = e, l = crypto.getRandomValues(new Uint8Array(8));
                     k = btoa(String.fromCharCode(...l));
                     let a = new URLSearchParams;
-                    a.append("build_id", "259fa349063c1fd443dd6a6373052e3a91a780bd"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
+                    a.append("build_id", "1b41c555d7750b4d275970a5baf08c9348bf4322"), a.append("rpc", String(t)), a.append("rpc_auth_token", k), n = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(a.toString())
                 },
                 OVERLAY_CALL_PRIVATE_CHANNEL: function(e) {
                     let {
@@ -1228,4 +1228,4 @@
         }
     }
 ]);
-//# sourceMappingURL=09bed4418dd084a58652.js.map
+//# sourceMappingURL=cd68dee2248cda54ad56.js.map
