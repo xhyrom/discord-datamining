6e54b67ccc876e601af47ca55e8b960f0c399397
xHyroM <github@xhyrom.dev>
2023-12-03 05:46:43 +0000

    📥 Public Testing Beta Web Build 250833 (84e28a2a0405b0add3743d16dfb186d672472b80)
    Build Number: 250833
    Version Hash: 84e28a2a0405b0add3743d16dfb186d672472b80
    Build At: 3/11/2023 5:39:40 (1701581980709)
    
    Scripts (107):
      shared.a8c0aa2a5e4113564c96.js
      app.6b083cb5dd59c1e44793.js
      3726.ba6a0e749bd825ff27a9.js
      5114.5e314a9ecea354b4126b.js
      21396.259a270b7e3f8803a333.js
      61160.b9c554bf37895ed48683.js
      22918.9f2b9d54bbfc371a4d92.js
      13798.5d1df801cfc219755d66.js
      63550.a619020e4c7b3d5be7ac.js
      482.6e1c86f88a37a71d42ec.js
      19263.fe32553ff71153cb7656.js
      10991.d742d0d238c0d99e96ae.js
      20117.7c4ea5cd4685b0442b9f.js
      31897.ec700144df6b20f401cb.js
      58409.1811376ebb7f14b0be53.js
      49554.34f5589cb37c5b3e60d3.js
      17134.052c763356b45a5e9049.js
      3341.1a1f8595a0c8fc9f99cf.js
      88790.cc022f06ade4cc9b31b8.js
      47470.ebcc7e56b847ac6fbd6a.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      25653.f1981721227784f0166e.js
      56072.d168431912c986199ba7.js
      65573.3d56d56002765d7a9da8.js
      46318.26a20b3d6c9d947ee7c5.js
      14875.4c8c0138c15590cddad0.js
      31717.3a43eb30b9802be446a5.js
      83554.2a246a1047818167dc16.js
      95121.e02bbfb29cfd5bef5284.js
      67079.8c773b08bd63e44275b8.js
      34426.aefa163dd8b10b7ac8cc.js
      41831.a3f3aaadb22451db89d8.js
      21251.68c5f799d8889a16996c.js
      27057.deae7e48af1346e56b16.js
      4082.2f77b9cbd858dafeb279.js
      67615.bf8b364ef35f56ab8e16.js
      89894.42fbeac85a824b63dae7.js
      80574.375b5be1da3c7307c49f.js
      8240.4afb2f5fca4959a4c51b.js
      69039.64566b959d0768184bdb.js
      18814.8a9d7b31f5a23de4ecfd.js
      37580.7d9656919e0c40a3856a.js
      99742.217a8e519977f9b5cbf0.js
      18407.7b9dba4b09d1f1505f8d.js
      47146.3787dacc286d6d9a7cc5.js
      98086.7cde3916a7cd7866ba4e.js
      21413.b53c032599ab3a5bcbf7.js
      75676.9ef6924d6296b0fafc32.js
      72870.d7e30a37a080f43af084.js
      64873.8aeb20cd344f2713758b.js
      4867.31dc0826609e48b39166.js
      74970.ad098636400bd7dcbe6c.js
      74836.b991877dde75f9619c99.js
      46493.1dc0baf17586aa900601.js
      94816.890d7b1dbae597f6aa4f.js
      48590.91e049a0d7e2ba0511cf.js
      37102.32ea61569c6da4c25583.js
      60499.9be9ce6effddbb435838.js
      79417.40e5966eb87d1ff2ee44.js
      22198.874b6ca9f8002a7bcb53.js
      49191.d845556dff27841ecd1e.js
      26230.a8a95f983d9bf704aebc.js
      17744.5de535b845b1936a62e7.js
      56145.1c4cecbf5c4af60f63d7.js
      58247.717a7faf458dde176e11.js
      59310.f88d66158bd597eabb5e.js
      35306.b8cb1ccc1b838a7a51bf.js
      62768.57c6a521ac7c20b1a75b.js
      80083.b0502c05d9127f49c1a6.js
      7105.da032cdf2d68f70cd9db.js
      66888.8e2d1bfde80e0a59906c.js
      23398.99046fdebb0d52a5294c.js
      77015.2496378d1ee6203d37ba.js
      97146.ba5d381e14f798be8b84.js
      82082.947fcc1e623381abadfd.js
      26737.36ed5a81390b304d18a5.js
      48449.79249c1b7c6d8d43a4f4.js
      81161.6dc5fd721981813137d3.js
      27043.105ce50242094adf158e.js
      24364.e8be85e45ddf2c8a1feb.js
      47498.38da6b2cf2f487359536.js
      48059.e10dfe5e2e8cc2a8c5f9.js
      43820.533cd67281812215cae3.js
      6086.1339c227c38e3c5b2586.js
      64612.26d2bf1afbde26a43a76.js
    * 18409.a2207db7facac2097bdb.js
      5593.5f67a9d65a72a0b7d15f.js
      28794.4204e337ff0879593f28.js
      33547.4c771f02a97eed59aa9a.js
      59459.f0458a563c6692b50b6e.js
      70941.6b9e92d78ec0a59b2052.js
      62597.1768a5537a0db3f9e8ac.js
      53509.d075f1bff85f12b95485.js
      53266.8e146b0315d0c6ec0577.js
      7273.fde16498afb14bd07b25.js
      65800.908344ac9d3dd1f6ae6d.js
      66318.a1f4b262d7219ef46b3b.js
      web.d3e126f9f6a9fe999c4f.js
      58409.1811376ebb7f14b0be53.js
      3341.1a1f8595a0c8fc9f99cf.js
      47470.ebcc7e56b847ac6fbd6a.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      25653.f1981721227784f0166e.js
      7273.fde16498afb14bd07b25.js
      sentry.550fa9fe212c41ea0856.js
    
    Stylesheets (2):
      shared.bfa0aac3246aef31d602.css
    * app.58978885ba1d0a3f9ce7.css

--- data/client/channels/ptb/web/scripts/main.js
+++ data/client/channels/ptb/web/scripts/main.js
@@ -247,8 +247,8 @@
                 return {
                     logsUploaded: new Date().toISOString(),
                     releaseChannel: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    buildNumber: "250831",
-                    versionHash: "50c0b3b165566762e16e2d73a65c249c26f95fe4"
+                    buildNumber: "250833",
+                    versionHash: "84e28a2a0405b0add3743d16dfb186d672472b80"
                 }
             }
             n.r(e), n.d(e, {
@@ -357,4 +357,4 @@
         }
     }
 ]);
-//# sourceMappingURL=18409.418f40ea8d4ac9df326a.js.map
+//# sourceMappingURL=18409.a2207db7facac2097bdb.js.map
