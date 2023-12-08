97d2348cabb8c72d9e5f98a61daa9918bd374628
xHyroM <github@xhyrom.dev>
2023-12-08 19:26:32 +0000

    📥 Public Testing Beta Web Build 252687 (84bfb00aca3f57a45168cbbbb69a46e84efaa542)
    Build Number: 252687
    Version Hash: 84bfb00aca3f57a45168cbbbb69a46e84efaa542
    Build At: 8/11/2023 19:19:37 (1702063177928)
    
    Scripts (106):
      shared.ffb9488d8d48f9a02fdf.js
      app.a6974e7914e10c233837.js
      3726.e3e95df69f8cf8ff7aa8.js
      51301.146fa2873f171856d3ad.js
      21396.259a270b7e3f8803a333.js
      61160.b9c554bf37895ed48683.js
      22918.9f2b9d54bbfc371a4d92.js
      13798.5d1df801cfc219755d66.js
      63550.a619020e4c7b3d5be7ac.js
      482.6e1c86f88a37a71d42ec.js
      19263.fe32553ff71153cb7656.js
      10991.d742d0d238c0d99e96ae.js
      20117.7c4ea5cd4685b0442b9f.js
      31897.ec700144df6b20f401cb.js
      58409.1811376ebb7f14b0be53.js
      49554.34f5589cb37c5b3e60d3.js
      69288.312b5f484a6756663ca9.js
      3341.1a1f8595a0c8fc9f99cf.js
      88790.cc022f06ade4cc9b31b8.js
      47470.c4ab7647d25b8ac58ca8.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      25653.f1981721227784f0166e.js
      13942.42b3309fce7f57e5eb63.js
      65573.3d56d56002765d7a9da8.js
      46318.26a20b3d6c9d947ee7c5.js
      14875.ab7170b144439cba21c6.js
      31717.3a43eb30b9802be446a5.js
      72870.d7e30a37a080f43af084.js
      83554.2a246a1047818167dc16.js
      47387.48123261786521d08785.js
      27057.ee8f9d6f6f76cab31011.js
      67079.8c773b08bd63e44275b8.js
      34426.aefa163dd8b10b7ac8cc.js
      41831.ad048c0163425aea4d2e.js
      43870.40da167473dc0e9edab1.js
      21251.68c5f799d8889a16996c.js
      48590.30373425cc01194cd313.js
      86480.1751f994c4d5178cc9c2.js
      67615.922a44ffa9c61ed78b7c.js
      89894.44f49c0018afb3694fe7.js
      8240.865c70ef34cec1e11192.js
      18814.8a9d7b31f5a23de4ecfd.js
      37580.640f5dece69c9284b1ba.js
      69039.0d3b4d5be7f6a21cf93d.js
      99742.217a8e519977f9b5cbf0.js
      18407.d0257553d76c1da19de7.js
      47146.57a6c7b40e47673fbb30.js
      98086.b4998038fa6ca144177e.js
      21413.04451f046a440f45bd24.js
      75676.8481ee3ef6c0d7c670c6.js
      64873.bd98aa3f1ec96684dac4.js
      35306.b8cb1ccc1b838a7a51bf.js
      74970.ad098636400bd7dcbe6c.js
      74836.b991877dde75f9619c99.js
      94491.6a88e419c71e384d3980.js
      94816.5a175ba33d20e66d2eca.js
      60499.862663374dc7b2606eb6.js
      37102.04489c88475d6b93636f.js
      79417.40e5966eb87d1ff2ee44.js
      49191.d845556dff27841ecd1e.js
      26230.a8a95f983d9bf704aebc.js
      17744.a586cfff7253ad1f7ffe.js
      22198.a2e33bdb57da1c7ef9b8.js
      59310.e3191416559a74901ac0.js
      56145.e45752c60fb418931ab5.js
      58247.6146ce4a4e300aea8b79.js
      62768.232f00c591becc89b0ca.js
      80083.8581aa78cb83cb4217b0.js
      7105.da032cdf2d68f70cd9db.js
      23398.99046fdebb0d52a5294c.js
      66888.e207ac8a6f2c14a9eaa9.js
      77015.cf81f4f7e14c534484d3.js
      97146.ba5d381e14f798be8b84.js
      82082.66eccead8076cc2a44a4.js
      26737.36ed5a81390b304d18a5.js
      48449.2187c818019636dbd7df.js
      81161.abe9578e4d66ca1934cd.js
      27043.105ce50242094adf158e.js
      24364.69cf16d98863af23f80e.js
      47498.38da6b2cf2f487359536.js
      48059.4498bce0d1c9ff3f4b8b.js
      43820.2dfe02f35ae2675fa737.js
      6086.1339c227c38e3c5b2586.js
      64612.26d2bf1afbde26a43a76.js
    * 18409.7a9b5d80331708c98418.js
      5593.33ef3abf7f73357d54cb.js
      55348.29e83ba0718684f2c9af.js
      45346.fda035387d99bd22d0ab.js
      70941.610eda6b5366c83284d8.js
      62597.1768a5537a0db3f9e8ac.js
      53509.d075f1bff85f12b95485.js
      47252.9752854393f3296529ef.js
      53526.ad1fa7caed5e70d3ddc2.js
      7273.48d411066ee415b308a7.js
      65800.657a577c4f6fc39eb27e.js
      66318.f13aa3f75b753cfa905a.js
      web.8c93caaebbc7f42976e1.js
      58409.1811376ebb7f14b0be53.js
      3341.1a1f8595a0c8fc9f99cf.js
      47470.c4ab7647d25b8ac58ca8.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      25653.f1981721227784f0166e.js
      7273.48d411066ee415b308a7.js
      sentry.fdd5b27d7cf56b4a354f.js
    
    Stylesheets (2):
      shared.f01e9143e31c19a68a8e.css
    * app.0c253b4634ffdbf72af7.css

--- data/client/channels/ptb/web/scripts/scripts/5593.33ef3abf7f73357d54cb.js
+++ data/client/channels/ptb/web/scripts/scripts/5593.33ef3abf7f73357d54cb.js
@@ -2375,7 +2375,7 @@
                     dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
                     autoSessionTracking: !1,
                     environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    release: "discord_web-dae74735474877b0f60ecf390cf284d3369807cb",
+                    release: "discord_web-84bfb00aca3f57a45168cbbbb69a46e84efaa542",
                     beforeSend: e => {
                         var t, n;
                         return !(null != (t = e).exception && null != t.exception.values && t.exception.values.every(e => null == e.stacktrace || null != e.stacktrace.frames && 1 === e.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || s.some(e => window.navigator.appVersion.toLowerCase().indexOf(e) >= 0)) && !i() && !("Aborted" === (n = e).message || "cancel captcha" === n.message) && l() ? e : null
@@ -2393,7 +2393,7 @@
                     })],
                     ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
                     denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-                }), a.setTag("buildNumber", (e = "252682", "252682")), a.setTag("builtAt", String("1702062797096"));
+                }), a.setTag("buildNumber", (e = "252687", "252687")), a.setTag("builtAt", String("1702063177928"));
                 let t = window.GLOBAL_ENV.SENTRY_TAGS;
                 if (null != t && "object" == typeof t)
                     for (let e in t) a.setTag(e, t[e]);
@@ -4914,4 +4914,4 @@
         }
     }
 ]);
-//# sourceMappingURL=5593.536c75973cfc3da631d5.js.map
+//# sourceMappingURL=5593.33ef3abf7f73357d54cb.js.map
