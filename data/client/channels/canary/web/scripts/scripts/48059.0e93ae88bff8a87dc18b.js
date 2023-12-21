833d869ec7df654440de1f584a43bd93bd2037b3
xHyroM <github@xhyrom.dev>
2023-12-21 17:18:43 +0000

    📥 Canary Web Build 256138 (5a0a3d144b66a01216d119b0e6be2ce8c0d5fda5)
    Build Number: 256138
    Version Hash: 5a0a3d144b66a01216d119b0e6be2ce8c0d5fda5
    Build At: 21/11/2023 17:00:03 (1703178003830)
    
    Scripts (104):
      shared.a4778bbadb17d3f89145.js
      app.56167a22e944e6a8a34f.js
      15959.49ed75a92403de5c74ea.js
      65000.236f6df6c89c0c45ac34.js
      38783.c50afef19be6696df507.js
      21396.259a270b7e3f8803a333.js
      17820.e883271a8a21d461b3cc.js
      22918.9f2b9d54bbfc371a4d92.js
      13798.6a2a5ac1a86675c94b6c.js
      63550.a619020e4c7b3d5be7ac.js
      482.6e1c86f88a37a71d42ec.js
      19263.fe32553ff71153cb7656.js
      10991.d742d0d238c0d99e96ae.js
      20117.7c4ea5cd4685b0442b9f.js
      58409.1811376ebb7f14b0be53.js
      31897.ec700144df6b20f401cb.js
      55695.7eba6a85898459a23ba1.js
      3341.1a1f8595a0c8fc9f99cf.js
      64999.3c0486790babc24c66a4.js
      47470.c4ab7647d25b8ac58ca8.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      53706.c13e855ace8621f4cbe9.js
      25653.f1981721227784f0166e.js
      72054.ccb12939fa7033320d60.js
      46318.26a20b3d6c9d947ee7c5.js
      14875.f66aa2d4bfb753904d95.js
      31717.0d22591a37153022cbab.js
      48590.1b76e1504f6a5bf1cfc3.js
      83554.cede2484fa93c9844fae.js
      47387.f38d121005d66b7eeee2.js
      30982.a6d605c291ff090be83b.js
      34426.9f82349d8cf165e1b07e.js
      41831.ad048c0163425aea4d2e.js
      43870.ad436802f4e3ebc9b214.js
      21251.7f6a30385c27c5b8c01f.js
      67079.8c773b08bd63e44275b8.js
      86480.ebf8826a7f33e22a6aba.js
      67615.e8beb331b7bee284dba9.js
      52428.81c432078c02c67f07a9.js
      8240.6e986e4934314ba8efa8.js
      37580.640f5dece69c9284b1ba.js
      18814.1d6a80b11d936b41c5d9.js
      69039.e145df55aac3ad30a4ce.js
      99742.217a8e519977f9b5cbf0.js
      18407.d0257553d76c1da19de7.js
      47146.3055dfc334424c70363e.js
      98086.48d1ce374b9e151c1e25.js
      21413.4df2838a904251745894.js
      75676.8481ee3ef6c0d7c670c6.js
      67820.89b724532f9991f7db9d.js
      35306.b8cb1ccc1b838a7a51bf.js
      74970.ad098636400bd7dcbe6c.js
      74836.b991877dde75f9619c99.js
      94491.1493d9cb04f8cb43be69.js
      94816.6a8b2e3e8dca2d49821b.js
      60499.862663374dc7b2606eb6.js
      37102.04489c88475d6b93636f.js
      79417.a01eecd15efd3a1d343e.js
      49191.4c47aae235ac3c0cdcd4.js
      17744.4690bcdfef03e80064c5.js
      22198.aecf178946716718966c.js
      23777.aecf683d1c5c7b63449c.js
      56145.965feeae8e31520fdcad.js
      58247.46790bed6486ddfd027d.js
      26230.0aca8135c4f2a26ac118.js
      62768.41de9bf94c0f314cc33e.js
      80083.7fd81fb4889aa662cd19.js
      7105.da032cdf2d68f70cd9db.js
      66888.e207ac8a6f2c14a9eaa9.js
      77015.64aac5de949f870e569d.js
      7704.768be05f0ebc44490f7c.js
      82082.8c0a20c07bebced7662d.js
      26737.36ed5a81390b304d18a5.js
      81161.ddb468615506c4a20690.js
      27043.105ce50242094adf158e.js
      72243.018493baa2c4b43da0cf.js
      47498.38da6b2cf2f487359536.js
      48059.0e93ae88bff8a87dc18b.js
      55639.f4b3715209ac7b2c4b6c.js
      64612.26d2bf1afbde26a43a76.js
      6086.272964ad8589a973eeaa.js
    * 18409.5cbb747e125cb7d7e5ab.js
      69628.eef67edd3564f3d42b0a.js
      75178.23afcaefc02b93d47226.js
      55348.aee1ea4755a2b7781dff.js
      45346.d1ad9c8ea227b9746eb3.js
      70941.610eda6b5366c83284d8.js
      1375.e96a5e7a7b3212a9d238.js
      53509.d075f1bff85f12b95485.js
      79997.3fbba3ae1736d2d7362f.js
      7273.00ec7254b2ce98dc759a.js
      93464.cbb6acc8337a79d665e9.js
      65800.657a577c4f6fc39eb27e.js
      14907.96cce708ad49fbf97042.js
      web.a2fb55889314033f4f79.js
      58409.1811376ebb7f14b0be53.js
      3341.1a1f8595a0c8fc9f99cf.js
      47470.c4ab7647d25b8ac58ca8.js
      22843.1bda3edd4dd152273661.js
      94288.dbd73ecb6b1482a870b7.js
      25653.f1981721227784f0166e.js
      7273.00ec7254b2ce98dc759a.js
      sentry.117b858d8e9e94ef309b.js
    
    Stylesheets (2):
      shared.6faf2904172526bcd88f.css
    * app.aa5ebf206729da65ce2c.css

--- data/client/channels/canary/web/scripts/scripts/48059.0e93ae88bff8a87dc18b.js
+++ data/client/channels/canary/web/scripts/scripts/48059.0e93ae88bff8a87dc18b.js
@@ -5320,7 +5320,7 @@
                 let i = {},
                     r = window.GLOBAL_ENV.RELEASE_CHANNEL;
                 r && (i.release_channel = r.split("-")[0]);
-                let o = parseInt((n = "256130", "256130"), 10);
+                let o = parseInt((n = "256138", "256138"), 10);
                 !isNaN(o) && (i.client_build_number = o);
                 let s = null == N ? void 0 : null === (e = (t = N.remoteApp).getBuildNumber) || void 0 === e ? void 0 : e.call(t);
                 return !isNaN(s) && (i.native_build_number = s), i.client_event_source = function() {
@@ -5562,4 +5562,4 @@
         }
     }
 ]);
-//# sourceMappingURL=48059.61e82dd23c99c8e05b9a.js.map
+//# sourceMappingURL=48059.0e93ae88bff8a87dc18b.js.map
