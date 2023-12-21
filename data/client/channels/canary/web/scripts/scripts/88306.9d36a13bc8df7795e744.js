7f1cda87f75cf61218e760bdd13da8fff8e94b65
xHyroM <github@xhyrom.dev>
2023-12-21 21:50:00 +0000

    📥 Canary Web Build 256256 (b237701f470d33f4de4d2620f90657e7ce2b16e2)
    Build Number: 256256
    Version Hash: b237701f470d33f4de4d2620f90657e7ce2b16e2
    Build At: 21/11/2023 21:42:54 (1703194974809)
    
    Scripts (66):
      shared.b605215fa278189bdfe3.js
      app.245755942a07548f0529.js
      437.7f3f7c545f948cdf58e5.js
      15875.9ada85ce37483c3d9408.js
      97084.911ce890bef83d01d2b8.js
      80204.86bd74ae149ea556b751.js
      68057.ba870cdc447b845ecaaa.js
      49852.de0d65ac8058a3f1b2d4.js
      72171.d9f6ad6a29144837958f.js
      43464.53e43039cd011d10ad5c.js
      65938.3dd36012c276cff5baeb.js
      51504.528c19e90384d6508978.js
      2069.bce651c37bbccd4ea045.js
      70446.79df401cc0195f64fdb0.js
      87226.eea3f0b1ece082d51eea.js
      82871.d3f8226509f932e471a6.js
      14700.d384d1298befc0eeff2e.js
      44529.dd3aaaa6164a7c15fadd.js
      32990.221af3afd72c3e6692d2.js
      92113.d9d79fb5ad2c5326d418.js
      84661.74052e16dbb1c381ce33.js
      32948.ab1073b9881e6feb9bfd.js
      6082.d42bb6a3d8fbe5450871.js
      59733.3c3414ebc61e0f338d8c.js
      26627.834e1206df349f038d88.js
      19005.9549938e1afa7c52a09b.js
      82269.cb93f1eb2cf33a238811.js
      73040.5a91b18891a111691982.js
      95903.1e382dd9df74a382f01b.js
      49326.8cb334032a76c68805db.js
      73938.8ba769f84fb72952e8d9.js
      44032.dafe713922d779e88473.js
      79335.79482f036bf7fc441292.js
      65877.64d904765684b415e903.js
      88306.9d36a13bc8df7795e744.js
      86173.a2d261fda45e9a913f2b.js
      10323.84d6c882c938f5d35ceb.js
      6598.2cf292c3e73e40adebaf.js
      50756.38f030abea810a429202.js
      67245.bd9267240422b1d56c07.js
      478.b9f231a825332f398396.js
      7018.10f38c2d9dec8453f9a5.js
      39260.c7987ca5d69640016519.js
      92869.b09391e1f234046f43b0.js
      78338.ff40ff01eec281562ee9.js
      45310.3d803e558a35859d0479.js
      13931.cfacdd6a80666b63465d.js
      3417.f7ccfa4e470e8a5a0e93.js
      61967.1f3748d95ef700502992.js
    * 95719.4b05a853fa4224d32f3a.js
      70464.0f4276a1a92784ad0dc1.js
      48068.dd917f80df89336f0035.js
      39958.7665a88b428cf7dd06b2.js
      58869.f9505efd31db2ff7f6f1.js
      web.4cc8dc5eaf092ebec90e.js
      80204.86bd74ae149ea556b751.js
      72171.d9f6ad6a29144837958f.js
      65938.3dd36012c276cff5baeb.js
      2069.bce651c37bbccd4ea045.js
      70446.79df401cc0195f64fdb0.js
      87226.eea3f0b1ece082d51eea.js
      82871.d3f8226509f932e471a6.js
      14700.d384d1298befc0eeff2e.js
      44529.dd3aaaa6164a7c15fadd.js
      70464.0f4276a1a92784ad0dc1.js
      sentry.190104934c643d842e4d.js
    
    Stylesheets (2):
      49237.45596bec95a6afcf410a.css
    * 99387.990a2ddc71cea3ee14a1.css

--- data/client/channels/canary/web/scripts/scripts/88306.9d36a13bc8df7795e744.js
+++ data/client/channels/canary/web/scripts/scripts/88306.9d36a13bc8df7795e744.js
@@ -2780,7 +2780,7 @@
                             o = crypto.getRandomValues(new Uint8Array(8));
                         $ = btoa((i = String).fromCharCode.apply(i, (0, u._)(o)));
                         var s = new URLSearchParams;
-                        s.append("build_id", "024b1904c1ffe756c17778bceb5b8b71c77710d4"), s.append("rpc", String(a)), s.append("rpc_auth_token", $), x = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(s.toString())
+                        s.append("build_id", "b237701f470d33f4de4d2620f90657e7ce2b16e2"), s.append("rpc", String(a)), s.append("rpc_auth_token", $), x = "".concat(location.protocol, "//").concat(location.host, "/overlay?").concat(s.toString())
                     },
                     OVERLAY_CALL_PRIVATE_CHANNEL: function(t) {
                         var i = t.channelId,
@@ -2942,4 +2942,4 @@
         }
     }
 ]);
-//# sourceMappingURL=88306.571d11757fe1bade4787.js.map
+//# sourceMappingURL=88306.9d36a13bc8df7795e744.js.map
