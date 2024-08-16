0b09f729bdd3d856e93953f19b611ebdbee0d52f
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-16 17:43:41 +0200

    feat: improve getting

--- src/modules/client/web_build/scripts/chunks.ts
+++ src/modules/client/web_build/scripts/chunks.ts
@@ -11,9 +11,12 @@ export async function getChunks(code: string) {
     const promises = matches.map(async (item) => {
       if (checksums.has(item[2]!)) return;
 
+      console.log(`Scraping chunk ${item[2]}`);
       const file = new File(item[2]! + ".js");
       const content = await file.content();
       if (!content) return;
+
+      console.log(`Chunk ${item[2]} has been scraped and contains body`);
 
       chunks.push(file);
       checksums.add(item[2]!);
@@ -22,9 +25,11 @@ export async function getChunks(code: string) {
     });
 
     await Promise.all(promises);
+    console.log(`Processed ${chunks.length} chunks so far.`);
   }
 
   await recursiveChunks(code);
 
+  console.log(`Total chunks processed: ${chunks.length}`);
   return chunks;
 }
