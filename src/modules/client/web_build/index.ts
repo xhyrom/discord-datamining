1439dce1a3e52cc754d5795e49eab96286bf4019
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-16 17:44:33 +0200

    fix: check correctly if up to date

--- src/modules/client/web_build/index.ts
+++ src/modules/client/web_build/index.ts
@@ -52,8 +52,7 @@ export class WebBuild implements Module {
     if (
       latestVersionHash &&
       currentVersionHash &&
-      latestVersionHash === currentVersionHash.version_hash &&
-      false
+      latestVersionHash === currentVersionHash.version_hash
     ) {
       console.log(
         `${this.#channel.name} Web Build of Client %s (%s) is up to date`,
