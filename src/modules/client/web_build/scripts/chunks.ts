2c1a2dc99dc519ebf801da678869034d7ef223c7
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-16 19:13:59 +0200

    fix: less logs

--- src/modules/client/web_build/scripts/chunks.ts
+++ src/modules/client/web_build/scripts/chunks.ts
@@ -11,12 +11,9 @@ export async function getChunks(code: string) {
     const promises = matches.map(async (item) => {
       if (checksums.has(item[2]!)) return;
 
-      console.log(`Scraping chunk ${item[2]}`);
       const file = new File(item[2]! + ".js");
       const content = await file.content();
       if (!content) return;
-
-      console.log(`Chunk ${item[2]} has been scraped and contains body`);
 
       chunks.push(file);
       checksums.add(item[2]!);
